5742378fef6d32306b5f25d37fa1a0f4465b707e
rydnr <rydnr@acm-sl.org>
2016-01-19 23:00:04 +0100

    Work on Graphite image
    - Updated to latest set-square and dry-wit

--- graphite/build-settings.sh
+++ graphite/build-settings.sh
@@ -1,6 +1,18 @@
-defineEnvVar LICENSE_FILE \
-             "The file with the license details" \
-             'LICENSE.gpl3';
-defineEnvVar COPYRIGHT_PREAMBLE_FILE \
-             "The file with the copyright preamble" \
-             'copyright-preamble.default.txt';
+defineEnvVar GRAPHITE_SECRET_KEY \
+             "The secret key for graphite" \
+             "${RANDOM_PASSWORD}";
+defineEnvVar GRAPHITE_TIME_ZONE \
+             "The time zone for Graphite" \
+             'Europe/Madrid';
+defineEnvVar GRAPHITE_DB_NAME \
+             "The database name of the Graphite database" \
+             "graphite";
+defineEnvVar GRAPHITE_DB_USERNAME \
+             "The database user to access the Graphite database" \
+             "graphite";
+defineEnvVar GRAPHITE_DB_PASSWORD \
+             "The password to access the Graphite database" \
+             "${RANDOM_PASSWORD}";
+defineEnvVar GRAPHITE_DB_HOST \
+             "The Graphite database host" \
+             'graphitedb';
