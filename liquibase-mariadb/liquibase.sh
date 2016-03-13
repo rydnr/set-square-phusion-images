e36c434358e312d3abf348e44ee1ba3b541b788d
rydnr <rydnr@acm-sl.org>
2016-03-13 16:52:44 +0100

    Phabricator image now working

--- liquibase-mariadb/liquibase.sh
+++ liquibase-mariadb/liquibase.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd /opt/liquibase
-shift
 echo "Running liquibase --changeLogFile=/changelogs/changelog.yml --driver=org.mariadb.jdbc.Driver $@"
 ./liquibase --changeLogFile=/changelogs/changelog.yml --driver=org.mariadb.jdbc.Driver "$@"
