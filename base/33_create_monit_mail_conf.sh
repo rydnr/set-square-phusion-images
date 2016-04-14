6c8cbf5bc555e0139d46c394b9a26b55cd34ed5d
rydnr <rydnr@acm-sl.org>
2016-04-14 05:46:23 +0200

    Enabling local SMTP service. Enabling Phabricator ssh service

---
+++ base/33_create_monit_mail_conf.sh
@@ -0,0 +1,90 @@
+#!/bin/bash dry-wit
+# Copyright 2015-today Automated Computing Machinery S.L.
+# Distributed under the terms of the GNU General Public License v3
+
+function usage() {
+cat <<EOF
+$SCRIPT_NAME
+$SCRIPT_NAME [-h|--help]
+(c) 2015-today Automated Computing Machinery S.L.
+    Distributed under the terms of the GNU General Public License v3
+
+Creates Monit configuration file to send alerts via email.
+
+Common flags:
+    * -h | --help: Display this message.
+    * -v: Increase the verbosity.
+    * -vv: Increase the verbosity further.
+    * -q | --quiet: Be silent.
+EOF
+}
+
+## Defines the requirements
+## dry-wit hook
+function defineReq() {
+  checkReq process-file.sh PROCESS_FILE_NOT_INSTALLED;
+}
+
+## Defines the errors
+## dry-wit hook
+function defineErrors() {
+  export INVALID_OPTION="Unrecognized option";
+  export PROCESSFILE_NOT_INSTALLED="process-file.sh is not installed";
+
+  ERROR_MESSAGES=(\
+    INVALID_OPTION \
+    PROCESSFILE_NOT_INSTALLED \
+  );
+
+  export ERROR_MESSAGES;
+}
+
+## Validates the input.
+## dry-wit hook
+function checkInput() {
+
+  local _flags=$(extractFlags $@);
+  local _flagCount;
+  local _currentCount;
+  logDebug -n "Checking input";
+
+  # Flags
+  for _flag in ${_flags}; do
+    _flagCount=$((_flagCount+1));
+    case ${_flag} in
+      -h | --help | -v | -vv | -q)
+         shift;
+         ;;
+      *) logDebugResult FAILURE "failed";
+         exitWithErrorCode INVALID_OPTION;
+         ;;
+    esac
+  done
+
+  logDebugResult SUCCESS "valid";
+}
+
+## Parses the input
+## dry-wit hook
+function parseInput() {
+
+  local _flags=$(extractFlags $@);
+  local _flagCount;
+  local _currentCount;
+
+  # Flags
+  for _flag in ${_flags}; do
+    _flagCount=$((_flagCount+1));
+    case ${_flag} in
+      -h | --help | -v | -vv | -q)
+         shift;
+         ;;
+    esac
+  done
+}
+
+## Main logic
+## dry-wit hook
+function main() {
+  process-file.sh -o ${MONIT_MAIL_TEMPLATE_FILE} ${MONIT_MAIL_OUTPUT_FILE}
+}
