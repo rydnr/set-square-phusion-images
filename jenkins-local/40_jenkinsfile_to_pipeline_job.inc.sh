fd07e61cde7580f95e46da2429d5c6cb079984b4
José San Leandro <jose.sanleandro@osoco.es>
2016-05-06 16:12:36 +0200

    Added first version of the Jenkins-local image

---
+++ jenkins-local/40_jenkinsfile_to_pipeline_job.inc.sh
@@ -0,0 +1,2 @@
+defineEnvVar WORKSPACE_FOLDER "The workspace folder" "/workspace";
+defineEnvVar JENKINSFILE "The Jenkinsfile file" '${WORKSPACE_FOLDER}/Jenkinsfile';
---
+++ jenkins-local/40_jenkinsfile_to_pipeline_job.sh
@@ -0,0 +1,139 @@
+j#!/bin/bash dry-wit
+# Copyright 2015-today Automated Computing Machinery S.L.
+# Distributed under the terms of the GNU General Public License v3
+
+function usage() {
+cat <<EOF
+$SCRIPT_NAME
+$SCRIPT_NAME [-h|--help]
+(c) 2016-today Automated Computing Machinery S.L.
+    Distributed under the terms of the GNU General Public License v3
+
+Creates a Jenkins job for a given Jenkinsfile.
+
+Common flags:
+    * -h | --help: Display this message.
+    * -v: Increase the verbosity.
+    * -vv: Increase the verbosity further.
+    * -q | --quiet: Be silent.
+EOF
+}
+
+## Defines the errors
+## dry-wit hook
+function defineErrors() {
+  export INVALID_OPTION="Unrecognized option";
+
+  ERROR_MESSAGES=(\
+    INVALID_OPTION \
+    WORKSPACE_FOLDER_IS_NOT_SPECIFIED \
+    WORKSPACE_FOLDER_DOES_NOT_EXIST \
+    WORKSPACE_IS_NOT_A_FOLDER \
+    CANNOT_READ_WORKSPACE_FOLDER \
+    CANNOT_ACCESS_WORKSPACE_FOLDER \
+    JENKINSFILE_IS_NOT_SPECIFIED \
+    JENKINSFILE_DOES_NOT_EXIST \
+    CANNOT_READ_JENKINSFILE \
+  );
+
+  export ERROR_MESSAGES;
+}
+
+## Validates the input.
+## dry-wit hook
+function checkInput() {
+
+  local _flags=$(extractFlags $@);
+  local _flagCount;
+  local _currentCount;
+  logDebug -n "Checking input";
+
+  # Flags
+  for _flag in ${_flags}; do
+    _flagCount=$((_flagCount+1));
+    case ${_flag} in
+      -h | --help | -v | -vv | -q)
+         shift;
+         ;;
+      *) logDebugResult FAILURE "failed";
+         exitWithErrorCode INVALID_OPTION;
+         ;;
+    esac
+  done
+
+  if [[ -z "${WORKSPACE}" ]]; then
+    exitWithErrorCode WORKSPACE_FOLDER_IS_NOT_SPECIFIED;
+  fi
+  if [[ ! -e "${WORKSPACE}" ]]; then
+    exitWithErrorCode WORKSPACE_FOLDER_DOES_NOT_EXIST;
+  fi
+  if [[ ! -d "${WORKSPACE}" ]]; then
+    exitWithErrorCode WORKSPACE_IS_NOT_A_FOLDER;
+  fi
+  if [[ ! -r "${WORKSPACE}" ]]; then
+    exitWithErrorCode CANNOT_READ_WORKSPACE_FOLDER;
+  fi
+  if [[ ! -x "${WORKSPACE}" ]]; then
+    exitWithErrorCode CANNOT_ACCESS_WORKSPACE_FOLDER;
+  fi
+  if [[ -z "${JENKINSFILE}" ]]; then
+    exitWithErrorCode JENKINSFILE_IS_NOT_SPECIFIED;
+  fi
+  if [[ -e "${JENKINSFILE}" ]]; then
+    exitWithErrorCode JENKINSFILE_DOES_NOT_EXIST;
+  fi
+  if [[ -r "${JENKINSFILE}" ]]; then
+    exitWithErrorCode CANNOT_READ_JENKINSFILE;
+  fi
+
+  logDebugResult SUCCESS "valid";
+}
+
+## Parses the input
+## dry-wit hook
+function parseInput() {
+
+  local _flags=$(extractFlags $@);
+  local _flagCount;
+  local _currentCount;
+
+  # Flags
+  for _flag in ${_flags}; do
+    _flagCount=$((_flagCount+1));
+    case ${_flag} in
+      -h | --help | -v | -vv | -q)
+         shift;
+         ;;
+    esac
+  done
+}
+
+## Main logic
+## dry-wit hook
+function main() {
+  local _jobHome="/var/jenkins_home/jobs/project";
+  local _config="${_jobHome}/config.xml";
+  mkdir -p "${_jobHome}";
+
+  cat <<EOF
+<?xml version='1.0' encoding='UTF-8'?>
+<flow-definition plugin="workflow-job@2.1">
+  <actions/>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@2.2">
+    <script>
+EOF > ${_config}
+  cat ${JENKINSFILE} >> ${_config}
+  cat <<EOF
+</script>
+    <sandbox>false</sandbox>
+  </definition>
+  <triggers/>
+</flow-definition>
+EOF
+  EOF > ${_config}
+
+  ln -s ${WORKSPACE_FOLDER} ${_jobHome}/workspace;
+}
