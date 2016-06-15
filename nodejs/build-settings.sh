f9ae1bc38b4173bda9c4fe9ad079eeb45a03bf54
Jose San Leandro <jose.sanleandro@osoco.es>
2016-06-15 13:25:00 +0200

    Added json5 as new module

--- nodejs/build-settings.sh
+++ nodejs/build-settings.sh
@@ -1,5 +1,5 @@
 defineEnvVar NODEJS_MAJOR_VERSION "The major version of NodeJS" "6";
 defineEnvVar NODEJS_DOWNLOAD_URL "The url to download NodeJS artifact" 'https://deb.nodesource.com/setup_${NODEJS_MAJOR_VERSION}.x';
-defineEnvVar NODEJS_MODULES "The NodeJS modules to install" "inherits uncss";
+defineEnvVar NODEJS_MODULES "The NodeJS modules to install" "inherits uncss json5";
 defineEnvVar SERVICE_USER "The name of the user" "dev";
 defineEnvVar SERVICE_GROUP "The name of the group" "users";
