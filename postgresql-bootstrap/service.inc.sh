fedf73d01b838054a34e2150aa7556a86a2e4fc9
Jose <chous@acm-sl.org>
2016-06-20 17:31:54 +0200

    Added schema to postgresql-bootstrap

--- postgresql-bootstrap/service.inc.sh
+++ postgresql-bootstrap/service.inc.sh
@@ -1,3 +1,4 @@
 defineEnvVar DB_HOST "The PostgreSQL host" "db";
 defineEnvVar SQL_FOLDER "The folder storing the SQL files to run" "/sql";
 defineEnvVar DEFAULT_DB_USER "The default database user" "root";
+defineEnvVar DEFAULT_DB_SCHEMA "The default database schema" "public";
