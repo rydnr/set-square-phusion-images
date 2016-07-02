b855c14ec805841a677220a6ff93c62749554aec
Jose <chous@acm-sl.org>
2016-07-02 22:29:23 +0200

    Improvements in nodejs, gradle and postgresql images

--- gradle/build-settings.sh
+++ gradle/build-settings.sh
@@ -1,4 +1,4 @@
-defineEnvVar GRADLE_VERSION "The Gradle version" "2.9";
+defineEnvVar GRADLE_VERSION "The Gradle version" "2.14";
 overrideEnvVar ENABLE_LOGSTASH 'false';
 defineEnvVar SERVICE_USER "The user account" "${NAMESPACE}";
 defineEnvVar SERVICE_GROUP "The group" "users";
