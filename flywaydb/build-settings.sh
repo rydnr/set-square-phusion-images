51ed34fac00df186b38243fbb50ff1fa37f3b9c6
Jose San Leandro <jose.sanleandro@osoco.es>
2016-07-22 18:49:05 +0200

    Fixed nodejs, gradle and flyway images

--- flywaydb/build-settings.sh
+++ flywaydb/build-settings.sh
@@ -1,3 +1,5 @@
 defineEnvVar FLYWAYDB_VERSION "The version of FlywayDB" "4.0.3";
 defineEnvVar FLYWAYDB_ARTIFACT "The FlywayDB artifact" 'flyway-commandline-${FLYWAYDB_VERSION}-linux-x64.tar.gz';
 defineEnvVar FLYWAYDB_URL "The url to download FlywayDB" 'https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/${FLYWAYDB_VERSION}/${FLYWAYDB_ARTIFACT}';
+defineEnvVar SERVICE_USER "The flyway user" "flyway";
+defineEnvVar SERVICE_GROUP "The flyway group" "flyway";
