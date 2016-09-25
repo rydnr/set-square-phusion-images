1a04194b971a778b94bd497c112efd84b7c86b3c
Jose <chous@acm-sl.org>
2016-09-25 20:28:46 +0200

    Upgraded logstash version.
    - Updated set-square version.
    - Updated dry-wit version.

--- java/build-settings.sh
+++ java/build-settings.sh
@@ -1,12 +1,7 @@
 defineEnvVar JAVA_VERSION "The version of the Java VM" "8";
-defineEnvVar BOUNCY_CASTLE_VERSION \
-             "The version of Bouncy Castle" \
-             "154";
-defineEnvVar BOUNCY_CASTLE_ARTIFACT \
-             "The Bouncy Castle artifact" \
-             'bcprov-jdk15on-${BOUNCY_CASTLE_VERSION}.jar';
-defineEnvVar BOUNCY_CASTLE_DOWNLOAD_URL \
-             "The Bouncy Castle download URL" \
-             'http://www.bouncycastle.org/download/${BOUNCY_CASTLE_ARTIFACT}';
-defineEnvVar LOGSTASH_VERSION "The Logstash version" "2.2";
+defineEnvVar BOUNCY_CASTLE_VERSION "The version of Bouncy Castle" "154";
+defineEnvVar BOUNCY_CASTLE_ARTIFACT "The Bouncy Castle artifact" 'bcprov-jdk15on-${BOUNCY_CASTLE_VERSION}.jar';
+defineEnvVar BOUNCY_CASTLE_DOWNLOAD_URL "The Bouncy Castle download URL" 'http://www.bouncycastle.org/download/${BOUNCY_CASTLE_ARTIFACT}';
+defineEnvVar LOGSTASH_VERSION "The Logstash version" "2.3";
 overrideEnvVar ENABLE_LOGSTASH true;
+
