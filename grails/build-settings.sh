c93d8f7482f4f2bde9cd5dd655cce745bf2ebb7e
Jose San Leandro <jose.sanleandro@osoco.es>
2016-10-19 17:10:23 +0200

    Downgrading grails to 3.1.9

--- grails/build-settings.sh
+++ grails/build-settings.sh
@@ -1,3 +1,3 @@
-defineEnvVar GRAILS_VERSION "The Grails version" "3.2.0";
+defineEnvVar GRAILS_VERSION "The Grails version" "3.1.9";
 defineEnvVar SERVICE_USER "The user account" "${NAMESPACE}";
 defineEnvVar SERVICE_GROUP "The group" "users";
