cb2a3e459b8ef8febb5c8664ad6bbd1b0c825883
Jose <chous@acm-sl.org>
2016-10-24 08:17:37 +0200

    base Dockerfile now fully refactored into templates

--- base/40_create_ssl_certificate_openssl.inc.sh
+++
@@ -1,7 +0,0 @@
-defineEnvVar SSL_CERTIFICATE_ALIAS "The alias of the SSL certificate" "${SSL_CERTIFICATE_ALIAS}";
-defineEnvVar SSL_KEY_ALGORITHM "The algorithm of the SSL key pairs" "rsa";
-defineEnvVar SSL_KEY_LENGTH "The length of the SSL key" "${SSL_KEY_LENGTH";
-defineEnvVar SSL_KEY_FOLDER "The folder storing the SSL key pairs" "/etc/ssl/private";
-defineEnvVar SSL_KEY_PASSWORD "The key password" "K8emqG04hZKlOUY3rET1ZBChtqFuERxyWAtyVtnVrP1MYMXhA8SPhdBJ";
-defineEnvVar SSL_CERTIFICATE_SUBJECT "The subject of the SSL certificate" "${SSL_CERTIFICATE_SUBJECT}";
-defineEnvVar SSL_CERTIFICATE_EXPIRATION_DAYS "The number of days until the certificate expires" "3650";
