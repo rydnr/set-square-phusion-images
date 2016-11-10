714a025cf9bc90b02ee683bd78bf323e5aed2688
Jose San Leandro <jose.sanleandro@osoco.es>
2016-11-10 09:26:33 +0100

    Some changes in nodejs image

--- nodejs/build-settings.sh
+++ nodejs/build-settings.sh
@@ -1,6 +1,6 @@
+defineEnvVar BASE_TAG "The base tag" "201611";
 defineEnvVar NODEJS_MAJOR_VERSION "The major version of NodeJS" "6";
-defineEnvVar NODEJS_DOWNLOAD_URL "The url to download NodeJS artifact" 'https://deb.nodesource.com/setup_${NODEJS_MAJOR_VERSION}.x';
-defineEnvVar NODEJS_MODULES "The NodeJS modules to install" "inherits uncss json5 eslint gh-pages mountebank";
+overrideEnvVar TAG '${NODEJS_MAJOR_VERSION}';
 defineEnvVar SERVICE_USER "The name of the user" '${NAMESPACE}';
 defineEnvVar SERVICE_GROUP "The name of the group" "users";
 overrideEnvVar ENABLE_RSNAPSHOT "false";
