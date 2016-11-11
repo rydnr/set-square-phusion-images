9e3a110518694a163cfe50e03a3800e0c498803b
Jose San Leandro <jose.sanleandro@osoco.es>
2016-11-11 14:16:24 +0100

    Refactored rabbitmq and jenkins images

--- gradle/build-settings.sh
+++ gradle/build-settings.sh
@@ -3,4 +3,6 @@ defineEnvVar GRADLE_VERSION "The Gradle version" "${TAG}";
 overrideEnvVar ENABLE_LOGSTASH 'false';
 defineEnvVar SERVICE_USER "The user account" "${NAMESPACE}";
 defineEnvVar SERVICE_GROUP "The group" "users";
+defineEnvVar SERVICE_USER_HOME "The home of the service user" '/home/${SERVICE_USER}';
+defineEnvVar SERVICE_USER_SHELL "The shell of the service user" '/bin/bash';
 defineEnvVar DEFAULT_DOCKER_API_VERSION "The docker API version" "1.23";
