0b22a02b3742a8583c85f44c6cf60843e86225e7
Jose <chous@acm-sl.org>
2016-11-21 09:11:51 +0100

    Minor changes in monit-port-check

--- .templates/monit-files/32_create_monit_port_check.sh
+++ .templates/monit-files/32_create_monit_port_check.sh
@@ -120,23 +120,25 @@ function extract_ports() {
 ## Main logic
 ## dry-wit hook
 function main() {
-  local _ports;
+  local _ports="";
+  local _oldIFS="${IFS}";
+
   echo 'check host localhost with address 0.0.0.0' > ${MONIT_CONF_FILE}
 
-  local _oldIFS="${IFS}";
   IFS=$' ';
   for d in ${DOCKERFILES_LOCATION}/*; do
     IFS="${_oldIFS}";
     extract_ports "${d}";
-    _ports="${RESULT}";
-    IFS=$' ';
-    for _port in ${_ports}; do
-      IFS="${_oldIFS}";
-      logInfo -n "Creating Monit check for ${_port} port (exposed in ${DOCKERFILES_LOCATION}/${d})";
-      cat <<EOF >> ${MONIT_CONF_FILE}
-	if failed port ${_port} with timeout ${PORT_TIMEOUT} then alert
+    _ports="${_ports} $(echo ${RESULT} | tr "\n" " ")";
+  done
+  _ports=$(echo ${_ports} | tr " " "\n" | sort | uniq | tr "\n" " ");
+  IFS=$' ';
+  for _port in ${_ports}; do
+    IFS="${_oldIFS}";
+    logInfo -n "Creating Monit check for ${_port} port (exposed in ${d})";
+    cat <<EOF >> ${MONIT_CONF_FILE}
+        if failed port ${_port} with timeout ${PORT_TIMEOUT} then alert
 EOF
-      logInfoResult SUCCESS "done";
-    done
+    logInfoResult SUCCESS "done";
   done
 }
