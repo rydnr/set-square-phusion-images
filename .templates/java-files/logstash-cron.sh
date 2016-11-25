778f56d13576cc71a1f6846459512b59465932e3
Jose San Leandro <jose.sanleandro@osoco.es>
2016-11-25 14:29:29 +0100

    Added metricbeat and packetbeat

--- .templates/java-files/logstash-cron.sh
+++ .templates/java-files/logstash-cron.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-exec /opt/logstash/bin/logstash agent -f /etc/logstash/conf.d/cron.conf
+if [ "${ENABLE_LOG STASH}" == "true" ]; then
+  exec logstash agent -f /etc/logstash/conf.d/cron.conf
+fi
