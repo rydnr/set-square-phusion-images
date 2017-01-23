91b9bed95f43ae97447cda67ee4c471908904cac
Jose <chous@acm-sl.org>
2017-01-23 08:33:36 +0100

    WIP

--- gradle-pharo/build-settings.sh
+++ gradle-pharo/build-settings.sh
@@ -1,3 +1,4 @@
-defineEnvVar PHARO_VERSION "The Pharo version" "4.0";
+defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "3.2.1";
+defineEnvVar PHARO_VERSION "The Pharo version" "5.0";
 defineEnvVar PHARO_VERSION_ZEROCONF "The Pharo version, as expected by the Zeroconf script in get.pharo.org" '$(echo ${PHARO_VERSION} | tr -d '.')';
 overrideEnvVar ENABLE_LOGSTASH 'false';
