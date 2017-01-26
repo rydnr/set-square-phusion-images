4c2ec297860582844d69163614dfcbcdab0c8dc9
Jose San Leandro <jose.sanleandro@osoco.es>
2017-01-26 15:01:08 +0100

    Some work on ElasticSearch. Updated Kibana to 5.1.2

--- elasticsearch/build-settings.sh
+++ elasticsearch/build-settings.sh
@@ -6,5 +6,3 @@ defineEnvVar SERVICE_USER "The ElasticSearch user" "elasticsearch";
 defineEnvVar SERVICE_GROUP "The ElasticSearch group" "elasticsearch";
 defineEnvVar SERVICE_USER_HOME "The home of ElasticSearch user" "/usr/share/elasticsearch";
 defineEnvVar SERVICE_USER_SHELL "The shell of ElasticSearch user" "/bin/bash";
-defineEnvVar BOOTSTRAP_MEMORYLOCK "The bootstrap.memory_lock setting in /etc/elasticsearch/elasticsearch.yml" "false";
-defineEnvVar DISCOVERY_ZEN_MINIMUMMASTERNODES "The discovery.zen.minimum_master_nodes setting in /etc/elasticsearch/elasticsearch.yml" "1";
