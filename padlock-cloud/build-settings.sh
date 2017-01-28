25761273673594e5bfb0de3f4e437fab121021b4
Jose <chous@acm-sl.org>
2017-01-28 21:08:51 +0100

    Pharo image working. Padlock Cloud image working. base-gui refactored

--- padlock-cloud/build-settings.sh
+++ padlock-cloud/build-settings.sh
@@ -1,5 +1,8 @@
 defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "201701";
 overrideEnvVar TAG "v1.0.0";
+defineEnvVar PADLOCK_CLOUD_DOMAIN "The padlock-cloud domain" '${DOMAIN}';
+defineEnvVar PADLOCK_CLOUD_VIRTUALHOST "The padlock-cloud virtual host" 'padlock.${PADLOCK_CLOUD_DOMAIN}';
 defineEnvVar SERVICE_USER "The Padlock-Cloud service user" "padlock";
 defineEnvVar SERVICE_GROUP "The Padlock-Cloud service group" "padlock";
 defineEnvVar SERVICE_USER_SHELL "The Padlock-Cloud account shell" "/bin/bash";
+defineEnvVar SERVICE_USER_HOME "The home of the Padlock-Cloud account" "/opt/padlock-cloud";
