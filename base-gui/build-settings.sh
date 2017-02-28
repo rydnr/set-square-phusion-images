2c7d6e537db84477b3e7cf39a7609f3281c20ae6
Jose <chous@acm-sl.org>
2017-02-28 21:46:11 +0100

    Docker image with gradle, pharo and git, for those Pharo enthusiasts that use xmonad in nixos and cannot find out why the vm crashes

--- base-gui/build-settings.sh
+++ base-gui/build-settings.sh
@@ -1,3 +1,4 @@
+defineEnvVar PARENT_IMAGE_TAG "The parent image tag" "201702";
 overrideEnvVar ENABLE_CRON "false";
 overrideEnvVar ENABLE_MONIT "false";
 overrideEnvVar ENABLE_RSNAPSHOT "false";
--- gradle-pharo/build-settings.sh
+++ gradle-pharo/build-settings.sh
@@ -1,6 +1,11 @@
-defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "201701";
+defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "201702";
 defineEnvVar GRADLE_VERSION "The Gradle version" '3.2.1';
 defineEnvVar PHARO_VERSION "The Pharo version" "5.0";
 overrideEnvVar TAG '${GRADLE_VERSION}-${PHARO_VERSION}';
 defineEnvVar PHARO_VERSION_ZEROCONF "The Pharo version, as expected by the Zeroconf script in get.pharo.org" '$(echo ${PHARO_VERSION} | tr -d '.')';
 overrideEnvVar ENABLE_LOGSTASH 'false';
+defineEnvVar WORKSPACE "The workspace folder" "/work";
+defineEnvVar SERVICE_USER "The service user" "pharo";
+defineEnvVar SERVICE_GROUP "The service group" "pharo";
+defineEnvVar SERVICE_USER_HOME "The home of the service user" "/home/pharo";
+defineEnvVar SERVICE_USER_SHELL "The shell of the service user" "/bin/bash";
