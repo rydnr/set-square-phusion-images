c1c47b8bf20412e7b3c618f58600797857ac2cbd
Jose San Leandro <jose.sanleandro@osoco.es>
2017-04-07 20:55:06 +0200

    Updated versions

--- base-gui/build-settings.sh
+++ base-gui/build-settings.sh
@@ -1,4 +1,4 @@
-defineEnvVar PARENT_IMAGE_TAG "The parent image tag" "201702";
+defineEnvVar PARENT_IMAGE_TAG "The parent image tag" "201704";
 overrideEnvVar ENABLE_CRON "false";
 overrideEnvVar ENABLE_MONIT "false";
 overrideEnvVar ENABLE_RSNAPSHOT "false";
--- gradle-pharo/build-settings.sh
+++ gradle-pharo/build-settings.sh
@@ -1,6 +1,6 @@
-defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "201702";
-defineEnvVar GRADLE_VERSION "The Gradle version" '3.2.1';
-defineEnvVar PHARO_VERSION "The Pharo version" "5.0";
+defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "201704";
+defineEnvVar GRADLE_VERSION "The Gradle version" '3.5-rc-3';
+defineEnvVar PHARO_VERSION "The Pharo version" "6.0";
 overrideEnvVar TAG '${GRADLE_VERSION}-${PHARO_VERSION}';
 defineEnvVar PHARO_VERSION_ZEROCONF "The Pharo version, as expected by the Zeroconf script in get.pharo.org" '$(echo ${PHARO_VERSION} | tr -d '.')';
 overrideEnvVar ENABLE_LOGSTASH 'false';
