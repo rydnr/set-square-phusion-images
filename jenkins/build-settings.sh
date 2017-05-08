0edf26eb83ebd23c0ea4a968e7cde843a9806a6f
Jose San Leandro <jose.sanleandro@osoco.es>
2017-05-08 17:14:21 +0200

    Some changes in jenkins

--- jenkins/build-settings.sh
+++ jenkins/build-settings.sh
@@ -1,5 +1,5 @@
 defineEnvVar PARENT_IMAGE_TAG "The version of the parent image" "201704";
-defineEnvVar JENKINS_VERSION "The Jenkins version" "2.57";
+defineEnvVar JENKINS_VERSION "The Jenkins version" "2.46.2";
 defineEnvVar TAG "The image tag" '${JENKINS_VERSION}';
 defineEnvVar SERVICE_USER "The service user" "jenkins";
 defineEnvVar SERVICE_GROUP "The service group" "jenkins";
