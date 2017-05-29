e7ec37c656de3db84f6422cc4eaf5f7b09da0c3b
Jose San Leandro <jose.sanleandro@osoco.es>
2017-05-29 10:05:29 +0200

    Added nodejs settings and tuned .gitignore

--- nodejs/build-settings.sh
+++ nodejs/build-settings.sh
@@ -1,9 +1,9 @@
-defineEnvVar PARENT_IMAGE_TAG "The base tag" "201702";
-defineEnvVar NODEJS_MAJOR_VERSION "The major version of NodeJS" "6";
+defineEnvVar PARENT_IMAGE_TAG "The base tag" "0.9.21";
+defineEnvVar NODEJS_MAJOR_VERSION "The major version of NodeJS" "7";
 overrideEnvVar TAG '${NODEJS_MAJOR_VERSION}';
 defineEnvVar SERVICE_USER "The name of the user" '${NAMESPACE}';
 defineEnvVar SERVICE_GROUP "The name of the group" "users";
-defineEnvVar SERVICE_USER_HOME "The home of the nodejs user" '/home/${NAMESPACE}';
+defineEnvVar SERVICE_USER_HOME "The home of the nodejs user" '/home/${SERVICE_USER}';
 defineEnvVar SERVICE_USER_SHELL "The shell of the nodejs user" "/bin/bash";
 overrideEnvVar ENABLE_RSNAPSHOT "false";
 overrideEnvVar ENABLE_LOGSTASH "false";
