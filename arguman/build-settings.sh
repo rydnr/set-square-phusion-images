836fbabea399af2d364b426136f6a3e6518b7107
Jose San Leandro <jose.sanleandro@osoco.es>
2017-07-18 11:38:58 +0200

    WIP arguman

--- arguman/build-settings.sh
+++ arguman/build-settings.sh
@@ -1,1 +1,5 @@
-defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "201611";
+defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "0.9.21";
+defineEnvVar SERVICE_USER "The service user" "apache";
+defineEnvVar SERVICE_GROUP "The service group" "apache";
+defineEnvVar SERVICE_USER_HOME "The home of the service user" '/home/${SERVICE_USER}';
+defineEnvVar SERVICE_USER_SHELL "The shell of the service user" "/bin/bash";
