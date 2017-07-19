acdcddf78109cd2a4d38db29177b2fc88669f56f
Jose San Leandro <jose.sanleandro@osoco.es>
2017-07-19 23:08:50 +0200

    Updated tomcat to tomcat 9 and jenkins to jenkins 2.70. Jenkins now works again

--- jenkins/build-settings.sh
+++ jenkins/build-settings.sh
@@ -1,7 +1,8 @@
 defineEnvVar PARENT_IMAGE_TAG "The version of the parent image" "0.9.21";
-defineEnvVar JENKINS_VERSION "The Jenkins version" "2.46.2";
+defineEnvVar JENKINS_VERSION "The Jenkins version" "2.70";
 defineEnvVar TAG "The image tag" '${JENKINS_VERSION}';
 defineEnvVar SERVICE_USER "The service user" "jenkins";
+defineEnvVar SERVICE_USER_PASSWORD "The jenkins password" "${RANDOM_PASSWORD}";
 defineEnvVar SERVICE_GROUP "The service group" "jenkins";
 defineEnvVar SERVICE_USER_HOME "The service user home" '/backup/jenkins-home';
 defineEnvVar SERVICE_USER_SHELL "The shell of the service user" "/bin/bash";
