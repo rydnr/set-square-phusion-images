9b2006a296513397ddc7ab5aff77aa749639139a
Jose San Leandro <jose.sanleandro@osoco.es>
2017-09-12 13:04:11 +0200

    Added --master to mongod in mongo image

--- mongodb/build-settings.sh
+++ mongodb/build-settings.sh
@@ -2,6 +2,7 @@ defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "0.9.22";
 defineEnvVar MONGODB_MAJOR_VERSION "The MongoDB version" "3.4";
 defineEnvVar MONGODB_VERSION "The MongoDB version" '${MONGODB_MAJOR_VERSION}.8';
 overrideEnvVar TAG '${MONGODB_VERSION}';
+defineEnvVar REPLICA_SET_NAME "The name of the replica set" '${NAMESPACE}';
 defineEnvVar SERVICE_USER "The service user" "mongodb";
 defineEnvVar SERVICE_USER_PASSWORD "The password of the service user" "${RANDOM_PASSWORD}";
 defineEnvVar SERVICE_GROUP "The service group" "mongodb";
