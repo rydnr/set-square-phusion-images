c93e07cf9350f4b876cc215c7f8918b3c212a9d2
Jose San Leandro <jose.sanleandro@osoco.es>
2017-09-20 15:14:25 +0200

    Minor changes in MongoDB image

--- .templates/mongodb-files/mongodb-dump.inc.sh
+++ .templates/mongodb-files/mongodb-dump.inc.sh
@@ -3,4 +3,4 @@ defineEnvVar MONGODB_USER "The MongoDB user" "";
 defineEnvVar MONGODB_PASSWORD 'The MongoDB password for ${MONGODB_USER}' "";
 defineEnvVar MONGODB_AUTHENTICATION_DATABASE 'The MongoDB authentication database for logging in as ${MONGODB_USER}' 'admin';
 defineEnvVar MONGODB_AUTHENTICATION_MECHANISM 'The MongoDB authentication mechanism for logging in as ${MONGODB_USER}' 'SCRAM-SHA-1';
-defineEnvVar MONGODB_DUMPS_FOLDER "The folder where the MongoDB dumps are stored" "/backup/mongodb/db/dumps";
+defineEnvVar MONGODB_DUMPS_FOLDER "The folder where the MongoDB dumps are stored" "/backup/mongodb/dumps";
