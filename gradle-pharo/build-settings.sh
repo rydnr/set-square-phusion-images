3fdc9290c81ea420af4f7b6e9c40d6a7a6f65f97
Jose San Leandro <jose.sanleandro@osoco.es>
2017-09-22 18:46:33 +0200

    Updated gradle version in gradle-pharo to 4.2

--- gradle-pharo/build-settings.sh
+++ gradle-pharo/build-settings.sh
@@ -1,10 +1,11 @@
 defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "0.9.21";
-defineEnvVar GRADLE_VERSION "The Gradle version" '4.0.1';
+defineEnvVar GRADLE_VERSION "The Gradle version" '4.2';
 defineEnvVar PHARO_VERSION "The Pharo version" "6.0";
 defineEnvVar TAG "The tag" '${GRADLE_VERSION}-${PHARO_VERSION}';
 defineEnvVar PHARO_VERSION_ZEROCONF "The Pharo version, as expected by the Zeroconf script in get.pharo.org" '$(echo ${PHARO_VERSION} | tr -d '.')';
 overrideEnvVar ENABLE_LOGSTASH 'false';
 defineEnvVar SERVICE_USER "The service user" "pharo";
+defineEnvVar SERVICE_USER_PASSWORD "The service user password" '${RANDOM_PASSWORD}';
 defineEnvVar SERVICE_GROUP "The service group" "pharo";
 defineEnvVar SERVICE_USER_HOME "The home of the service user" "/home/pharo";
 defineEnvVar SERVICE_USER_SHELL "The shell of the service user" "/bin/bash";
