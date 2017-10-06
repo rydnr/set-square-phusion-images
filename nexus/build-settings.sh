772b8d05f7f1eff3cf71c7121eace2a7029ff102
Jose San Leandro <jose.sanleandro@osoco.es>
2017-10-06 12:54:09 +0200

    Updated nexus to 3.6

--- nexus/build-settings.sh
+++ nexus/build-settings.sh
@@ -1,5 +1,5 @@
 defineEnvVar PARENT_IMAGE_TAG "The version of the parent image" "0.9.21";
-defineEnvVar NEXUS_VERSION "The version of Sonatype Nexus" "3.3.1-01";
+defineEnvVar NEXUS_VERSION "The version of Sonatype Nexus" "3.6";
 defineEnvVar TAG "The image tag" '${NEXUS_VERSION}';
 defineEnvVar NEXUS_ARTIFACT \
              "The Nexus artifact" \
