a2166f6b173e4a70717a522f6bbd7ba1fdd3ad1e
Jose San Leandro <jose.sanleandro@osoco.es>
2018-08-26 08:54:51 +0200

    Fixed arguman vulnerability

--- rundeck/build-settings.sh
+++ rundeck/build-settings.sh
@@ -1,14 +1,21 @@
 defineEnvVar RUNDECK_VERSION "The Rundeck version" "3.0.2" "wget -o /dev/null -O- http://dl.bintray.com/rundeck/rundeck-deb/ | grep deb | tail -n 1 | sed 's <.\?pre>  g' | cut -d '>' -f 2 | cut -d '<' -f 1 | sed 's ^rundeck-  g' | sed 's_\.deb$__g'";
 defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "0.11";
 overrideEnvVar TAG '${RUNDECK_VERSION}';
-overrideEnvVar GIT_TAG 'v${RUNDECK_VERSION}';d
-defineEnvVar DEFAULT_ADMIN_USER "The Rundeck admin user, by default" "admin";
+overrideEnvVar GIT_TAG 'v${RUNDECK_VERSION}';
+defineEnvVar DEFAULT_VIRTUAL_HOST "The Rundeck virtual host, by default" 'rundeck.${DOMAIN}';
+defineEnvVar VIRTUAL_HOST "The Rundeck virtual host" '${DEFAULT_VIRTUAL_HOST}';
+defineEnvVar DEFAULT_VIRTUAL_PORT "The default port of the Rundeck virtual host, by default" '8080';
+defineEnvVar VIRTUAL_PORT "The port of the Rundeck virtual host" '${DEFAULT_VIRTUAL_PORT}';
+defineEnvVar DEFAULT_ADMIN_USERNAME "The Rundeck admin user, by default" "admin";
 defineEnvVar DEFAULT_ADMIN_PASSWORD "The Rundeck admin password, by default" "secret" "${RANDOM_PASSWORD}";
 defineEnvVar SERVICE_USER "The service user" "rundeck";
 defineEnvVar SERVICE_GROUP "The service group" "rundeck";
-defineEnvVar SERVICE_USER_HOME "The home of the service user" '/home/${SERVICE_USER}';
+defineEnvVar SERVICE_USER_HOME "The home of the service user" '/var/lib/${SERVICE_USER}';
 defineEnvVar SERVICE_USER_SHELL "The shell of the service user" "/bin/bash";
 defineEnvVar DEFAULT_LOCALE "The default locale" "en_US";
 defineEnvVar DEFAULT_ENCODING "The default encoding" "UTF-8";
 defineEnvVar DEFAULT_JAVA_OPTS "The default JAVA_OPTS" "";
+defineEnvVar LOGSTASH_INPUT_PLUGINS "The space-separated list of Logstash input plugins" "logstash-input-log4j logstash-input-stdin";
+defineEnvVar LOGSTASH_FILTER_PLUGINS "The space-separated list of Logstash filter plugins" "logstash-filter-date logstash-filter-json";
+defineEnvVar LOGSTASH_OUTPUT_PLUGINS "The space-separated list of Logstash output plugins" "logstash-output-stdout";
 #
