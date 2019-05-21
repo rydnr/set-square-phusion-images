5ebfb55e137cc41feada9d227d15774b1691a7ae
josesl <jose.sanleandro@osoco.es>
2019-05-21 07:35:22 +0200

    Upgraded some arguman dependencies

--- arguman/build-settings.sh
+++ arguman/build-settings.sh
@@ -1,5 +1,6 @@
-defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "0.9.21";
-defineEnvVar SERVICE_USER "The service user" "apache";
-defineEnvVar SERVICE_GROUP "The service group" "apache";
-defineEnvVar SERVICE_USER_HOME "The home of the service user" '/home/${SERVICE_USER}';
-defineEnvVar SERVICE_USER_SHELL "The shell of the service user" "/bin/bash";
+defineEnvVar PARENT_IMAGE_TAG MANDATORY "The tag of the parent image" "0.11";
+defineEnvVar SERVICE_USER MANDATORY "The service user" "apache";
+defineEnvVar SERVICE_GROUP MANDATORY "The service group" "apache";
+defineEnvVar SERVICE_USER_HOME MANDATORY "The home of the service user" '/home/${SERVICE_USER}';
+defineEnvVar SERVICE_USER_SHELL MANDATORY "The shell of the service user" "/bin/bash";
+#
