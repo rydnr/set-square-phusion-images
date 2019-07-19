74f4ca543d7ee9c1f032d69b039a36b8b9b000ce
josesl <jose.sanleandro@osoco.es>
2019-07-19 11:08:45 +0200

    Updated mongodb image to 4.0.10

--- mongodb/build-settings.sh
+++ mongodb/build-settings.sh
@@ -1,10 +1,11 @@
-defineEnvVar PARENT_IMAGE_TAG "The tag of the parent image" "0.9.22";
-defineEnvVar MONGODB_MAJOR_VERSION "The MongoDB version" "3.4";
-defineEnvVar MONGODB_VERSION "The MongoDB version" '${MONGODB_MAJOR_VERSION}.8';
+defineEnvVar PARENT_IMAGE_TAG MANDATORY "The tag of the parent image" "0.11";
+defineEnvVar MONGODB_MAJOR_VERSION MANDATORY "The MongoDB version" "4.0";
+defineEnvVar MONGODB_VERSION MANDATORY "The MongoDB version" '${MONGODB_MAJOR_VERSION}.10';
 overrideEnvVar TAG '${MONGODB_VERSION}';
-defineEnvVar REPLICA_SET_NAME "The name of the replica set" '${NAMESPACE}';
-defineEnvVar SERVICE_USER "The service user" "mongodb";
-defineEnvVar SERVICE_USER_PASSWORD "The password of the service user" "${RANDOM_PASSWORD}";
-defineEnvVar SERVICE_GROUP "The service group" "mongodb";
-defineEnvVar SERVICE_USER_HOME "The home of the service user" '/backup/${IMAGE}';
-defineEnvVar SERVICE_USER_SHELL "The shell of the service user" "/bin/bash";
+defineEnvVar REPLICA_SET_NAME MANDATORY "The name of the replica set" '${NAMESPACE}';
+defineEnvVar SERVICE_USER MANDATORY "The service user" "mongodb";
+defineEnvVar SERVICE_USER_PASSWORD MANDATORY "The password of the service user" "${RANDOM_PASSWORD}";
+defineEnvVar SERVICE_GROUP MANDATORY "The service group" "mongodb";
+defineEnvVar SERVICE_USER_HOME MANDATORY "The home of the service user" '/backup/${IMAGE}';
+defineEnvVar SERVICE_USER_SHELL MANDATORY "The shell of the service user" "/bin/bash";
+#
