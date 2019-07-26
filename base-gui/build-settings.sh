343777b6c543b1405c100b83d27a6e6a64086c20
josesl <jose.sanleandro@osoco.es>
2019-07-26 09:58:30 +0200

    Minor fix in base-gui

--- base-gui/build-settings.sh
+++ base-gui/build-settings.sh
@@ -1,5 +1,5 @@
 defineEnvVar PARENT_IMAGE_TAG MANDATORY "The parent image tag" "0.11";
-defineEnvVar TAG 'the tag' '${PARENT_IMAGE_TAG}';
+defineEnvVar TAG MANDATORY 'the tag' '${PARENT_IMAGE_TAG}';
 overrideEnvVar ENABLE_CRON MANDATORY "false";
 overrideEnvVar ENABLE_MONIT MANDATORY "false";
 overrideEnvVar ENABLE_RSNAPSHOT MANDATORY "false";
