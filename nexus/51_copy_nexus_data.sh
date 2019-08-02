7da0a55c4015b220d131111458405c4edc0ad928
Jose San Leandro <jose.sanleandro@osoco.es>
2019-08-02 12:20:58 +0200

    Updated Nexus scripts to dry-wit 2

--- nexus/51_copy_nexus_data.sh
+++ nexus/51_copy_nexus_data.sh
@@ -1,93 +1,32 @@
 #!/bin/bash dry-wit
 # Copyright 2015-today Automated Computing Machinery S.L.
 # Distributed under the terms of the GNU General Public License v3
+# mod: nexus/51_copy_nexus_data
+# api: public
+# txt: Copies Nexus data to the correct location.
 
-function usage() {
-cat <<EOF
-$SCRIPT_NAME
-$SCRIPT_NAME [-h|--help]
-(c) 2015-today Automated Computing Machinery S.L.
-    Distributed under the terms of the GNU General Public License v3
-
-Copies Nexus data to the correct location.
-
-Common flags:
-    * -h | --help: Display this message.
-    * -v: Increase the verbosity.
-    * -vv: Increase the verbosity further.
-    * -q | --quiet: Be silent.
-EOF
-}
-
-## Defines the errors
-## dry-wit hook
-function defineErrors() {
-  export INVALID_OPTION="Unrecognized option";
-
-  ERROR_MESSAGES=(\
-    INVALID_OPTION \
-  );
-
-  export ERROR_MESSAGES;
-}
-
-## Validates the input.
-## dry-wit hook
-function checkInput() {
-
-  local _flags=$(extractFlags $@);
-  local _flagCount;
-  local _currentCount;
-  logDebug -n "Checking input";
-
-  # Flags
-  for _flag in ${_flags}; do
-    _flagCount=$((_flagCount+1));
-    case ${_flag} in
-      -h | --help | -v | -vv | -q)
-         shift;
-         ;;
-      *) logDebugResult FAILURE "failed";
-         exitWithErrorCode INVALID_OPTION;
-         ;;
-    esac
-  done
-
-  logDebugResult SUCCESS "valid";
-}
-
-## Parses the input
-## dry-wit hook
-function parseInput() {
-
-  local _flags=$(extractFlags $@);
-  local _flagCount;
-  local _currentCount;
-
-  # Flags
-  for _flag in ${_flags}; do
-    _flagCount=$((_flagCount+1));
-    case ${_flag} in
-      -h | --help | -v | -vv | -q)
-         shift;
-         ;;
-    esac
-  done
-}
-
-## Main logic
-## dry-wit hook
+# fun: main
+# api: public
+# txt: Copies Nexus data to the correct location.
+# txt: Returns 0/TRUE always.
+# use: main
 function main() {
-  rsync -avz /backup/nexus-conf/ /opt/sonatype/nexus/etc/
-  rsync -avz /backup/nexus-conf/ /sonatype-work/etc/
-  rsync -avz /opt/sonatype/nexus/etc/ /backup/nexus-conf/
-  rm -rf /opt/sonatype/nexus/etc /sonatype-work/etc
-  ln -s /backup/nexus-conf /opt/sonatype/nexus/etc
-  ln -s /backup/nexus-conf /sonatype-work/etc
-  rm -rf /sonatype-work/blobs
-  ln -s /backup/nexus-blobs /sonatype-blobs
-  ln -s /backup/nexus-blobs /sonatype-work/blobs
-  ln -s /backup/nexus-db /sonatype-work/db
+  rsync -avz /backup/nexus-conf/ /opt/sonatype/nexus/etc/ > /dev/null 2>&1
+  rsync -avz /backup/nexus-conf/ /sonatype-work/etc/ > /dev/null 2>&1
+  rsync -avz /opt/sonatype/nexus/etc/ /backup/nexus-conf/ > /dev/null 2>&1
+  rm -rf /opt/sonatype/nexus/etc /sonatype-work/etc > /dev/null 2>&1
+  ln -s /backup/nexus-conf /opt/sonatype/nexus/etc > /dev/null 2>&1
+  ln -s /backup/nexus-conf /sonatype-work/etc > /dev/null 2>&1
+  rm -rf /sonatype-work/blobs > /dev/null 2>&1
+  ln -s /backup/nexus-blobs /sonatype-blobs > /dev/null 2>&1
+  ln -s /backup/nexus-blobs /sonatype-work/blobs > /dev/null 2>&1
+  ln -s /backup/nexus-db /sonatype-work/db > /dev/null 2>&1
   /etc/my_init.d/00_chown_volumes.sh
-  chown -R ${SERVICE_USER}:${SERVICE_GROUP} /opt/sonatype /sonatype-work
+  chown -R ${SERVICE_USER}:${SERVICE_GROUP} /opt/sonatype /sonatype-work > /dev/null 2>&1
 }
+
+## Script metadata and CLI options
+
+setScriptDescription "Copies Nexus data to the correct location.";
+
+# vim: syntax=sh ts=2 sw=2 sts=4 sr noet
