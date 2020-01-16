b91183836ae5b321caad406e0e334c5a36af00a4
Jose San Leandro <josesla@ext.inditex.com>
2020-01-16 07:22:57 +0100

    Migrated remote-git-version.sh to dry-wit 2

--- .templates/check-version-files/remote-git-version.sh
+++ .templates/check-version-files/remote-git-version.sh
@@ -9,35 +9,33 @@ setScriptDescription "Retrieves the latest version of a remote git repository, e
 
 addError EITHER_LOCAL_OR_REMOTE_MUST_BE_SPECIFIED "Either -l or -r must be specified";
 addError LOCAL_REPOSITORY_DOES_NOT_EXIST "The local repository ${LOCAL_REPOSITORY} does not exist";
-addError CANNOT_RETRIEVE_HEAD_FROM_LOCAL_REPOSITORY "Could not retrieve the HEAD hash of the remote ${BRANCH} branch in ${LOCAL_REPOSITORY}";
+addError CANNOT_RETRIEVE_HEAD_FROM_LOCAL_REPOSITORY "Could not retrieve the HEAD hash of ${BRANCH} in ${LOCAL_REPOSITORY}";
 addError ERROR_ANALYZING_REMOTE_URL "Error analyzing the remote URL ${REMOTE_URL}";
 addError CANNOT_RETRIEVE_HEAD_FROM_REMOTE_URL "Could not retrieve the HEAD hash of the ${BRANCH} branch in ${REMOTE_URL}";
 
+addCommandLineFlag "local" "l" "Whether to retrieve the version using a local repository" OPTIONAL EXPECTS_ARGUMENT "";
+addCommandLineFlag "remote" "r" "Whether to retrieve the version using a url" OPTIONAL EXPECTS_ARGUMENT "";
+addCommandLineParameter "branch" "The branch" MANDATORY SINGLE;
+
+function dw_parse_local_cli_flag() {
+  export LOCAL_REPOSITORY="${1}";
+  export LOCAL_FLAG="${TRUE}";
+}
+
+function dw_parse_remote_cli_flag() {
+  export REMOTE_URL="${1}";
+  export REMOTE_FLAG="${TRUE}";
+}
+
+function dw_parse_branch_cli_parameter() {
+  export BRANCH="${1}";
+}
+
 ## Validates the input.
 ## dry-wit hook
-function checkInput() {
+function dw_checkInput() {
 
-  local _flags=$(extractFlags $@);
-  local _flagCount;
-  local _currentCount;
-  logDebug -n "Checking input";
-
-  # Flags
-  for _flag in ${_flags}; do
-    _flagCount=$((_flagCount+1));
-    case ${_flag} in
-      -h | --help | -v | -vv | -q | -l | --local | -r | --remote )
-         shift;
-         ;;
-      --)
-        shift;
-        break;
-        ;;
-      *) logDebugResult FAILURE "failed";
-         exitWithErrorCode INVALID_OPTION;
-         ;;
-    esac
-  done
+  logDebug -n "Validating input";
 
   if    isTrue "${LOCAL_FLAG}" \
      || isTrue "${REMOTE_FLAG}"; then
@@ -65,52 +63,16 @@ function checkInput() {
   logDebugResult SUCCESS "valid";
 }
 
-## Parses the input
-## dry-wit hook
-function parseInput() {
-
-  local _flags=$(extractFlags $@);
-  local _flagCount;
-  local _currentCount;
-
-  # Flags
-  for _flag in ${_flags}; do
-    _flagCount=$((_flagCount+1));
-    case ${_flag} in
-      -h | --help | -v | -vv | -q)
-         shift;
-         ;;
-      --)
-        shift;
-        break;
-        ;;
-      -l | --local)
-        shift;
-        LOCAL_REPOSITORY="${1}";
-        LOCAL_FLAG=${TRUE};
-        shift;
-        ;;
-      -r | --remote)
-        shift;
-        REMOTE_URL="${1}";
-        REMOTE_FLAG=${TRUE};
-        shift;
-        ;;
-    esac
-  done
-
-  BRANCH="${1}";
-  shift;
-}
-
 # fun: main
 # api: public
-# txt: 
-# txt: Returns 0/TRUE always.
+# txt: Prints the git version of the remote repository on standard output.
+# txt: Returns 0/TRUE if the version could be retrieved; an error otherwise.
 # use: main
 function main() {
   local _result;
   local _hash;
+
+  DW.import git;
 
   if isTrue "${LOCAL_FLAG}"; then
     logDebug -n "Retrieving HEAD for remote ${BRANCH} in ${LOCAL_REPOSITORY}";
