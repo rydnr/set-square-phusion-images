8a69a0a181993b1ea6c25338361fb4bfe3cc42b1
Jose San Leandro <josesla@ext.inditex.com>
2020-03-02 09:13:24 +0100

    Updated Artifactory image to 7.2.1

--- artifactory/build-settings.sh
+++ artifactory/build-settings.sh
@@ -1,8 +1,10 @@
-defineEnvVar ARTIFACTORY_VERSION MANDATORY "The version of Artifactory" "4.2.1" \
-    "curl -s -k http://dl.bintray.com/jfrog/artifactory/ | grep zip | grep -v asc | tail -n 1 | cut -d'\"' -f 4 | cut -d'-' -f 4 | sed 's_.zip__g'";
-defineEnvVar ARTIFACTORY_FILE MANDATORY "The Artifactory zip file" \
-    'jfrog-artifactory-oss-${ARTIFACTORY_VERSION}.zip';
-defineEnvVar ARTIFACTORY_DOWNLOAD_URL MANDATORY "The url to download Artifactory" \
-    'https://bintray.com/artifact/download/jfrog/artifactory/${ARTIFACTORY_FILE}';
-defineEnvVar TOMCAT_HOME "The home folder for Tomcat user" "/opt/tomcat";
+defineEnvVar PARENT_IMAGE_TAG MANDATORY "The version of the parent image" "0.11";
+defineEnvVar ARTIFACTORY_VERSION MANDATORY "The version of Artifactory" "7.2.1";
+defineEnvVar TAG MANDATORY "The image tag" '${ARTIFACTORY_VERSION}';
+defineEnvVar SERVICE_USER MANDATORY "The service user" "artifactory";
+defineEnvVar SERVICE_USER_PASSWORD MANDATORY "The artifactory password" "${RANDOM_PASSWORD}";
+defineEnvVar SERVICE_GROUP MANDATORY "The service group" "artifactory";
+defineEnvVar SERVICE_USER_HOME MANDATORY "The service user home" '/home/artifactory';
+defineEnvVar SERVICE_USER_SHELL MANDATORY "The shell of the service user" "/bin/bash";
+overrideEnvVar ENABLE_LOGSTASH true;
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
