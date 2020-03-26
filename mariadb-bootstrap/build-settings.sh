a21b2b7335739008c1f1c788699dcb585d139593
Jose San Leandro <josesla@ext.inditex.com>
2020-03-26 21:53:33 +0100

    Some work on mariadb-bootstrap image

--- mariadb-bootstrap/build-settings.sh
+++ mariadb-bootstrap/build-settings.sh
@@ -1,5 +1,5 @@
 defineEnvVar PARENT_IMAGE_TAG MANDATORY "The version of the parent image" "0.11";
-defineEnvVar UBUNTU_VERSION MANDATORY "The version available in Ubuntu" "$(docker run --rm -it ${REGISTRY}/${NAMESPACE}/base:${PARENT_IMAGE_TAG} remote-ubuntu-version mariadb-client | sed 's/[^0-9a-zA-Z\._-]//g')";
+defineEnvVar UBUNTU_VERSION MANDATORY "The version available in Ubuntu" "$(docker run --rm -it ${BASE_IMAGE_64BIT}:${PARENT_IMAGE_TAG} remote-ubuntu-version mariadb-client | sed 's/^.*://g' | sed 's/[^0-9a-zA-Z\._-]//g')";
 overrideEnvVar TAG '${UBUNTU_VERSION}';
 overrideEnvVar ENABLE_CRON "false";
 overrideEnvVar ENABLE_MONIT "false";
