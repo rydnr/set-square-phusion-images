d4f8e8ffc77b0ab3157f3209b6d713fc45d3ba0e
Jose San Leandro <josesla@ext.inditex.com>
2020-03-26 21:49:25 +0100

    Fixed bug in 02_change_default_locale.sh

--- .templates/locale-files/02_change_default_locale.sh
+++ .templates/locale-files/02_change_default_locale.sh
@@ -5,15 +5,15 @@
 # api: public
 # txt: Changes the system's default locale.
 
-DW.import locale;
-
 # fun: main
 # api: public
 # txt: Changes the locale and encoding (available after re-login).
 # txt: Returns 0/TRUE always.
 # use: main
 function main() {
-  change_default_locale "${LOCALE}" "${ENCODING}";
+  DW.import locale;
+
+  changeDefaultLocale "${LOCALE}" "${ENCODING}";
 }
 
 ## Script metadata and CLI settings.
@@ -30,5 +30,5 @@ function dw_parse_encoding_cli_flag() {
   export ENCODING="${1}";
 }
 
-addError "LOCALE_ENCODING_PAIR_IS_NOT_SUPPORTED" "The provided locale-encoding pair is not supported";
+addError LOCALE_ENCODING_PAIR_IS_NOT_SUPPORTED "The provided locale-encoding pair is not supported";
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
