e02689310f73e646a54972d86d5c5f37682cc13f
Jose San Leandro <josesla@ext.inditex.com>
2020-03-26 21:52:50 +0100

    Updated mariadb image

--- mariadb/build-settings.sh
+++ mariadb/build-settings.sh
@@ -1,5 +1,5 @@
 defineEnvVar PARENT_IMAGE_TAG MANDATORY "The version of the parent image" "0.11";
-defineEnvVar UBUNTU_VERSION MANDATORY "The version available in Ubuntu" "$(docker run --rm -it ${REGISTRY}/${NAMESPACE}/base:${PARENT_IMAGE_TAG} remote-ubuntu-version mariadb-server | sed 's/[^0-9a-zA-Z\._-]//g')";
+defineEnvVar UBUNTU_VERSION MANDATORY "The version available in Ubuntu" "$(docker run --rm -it ${BASE_IMAGE_64BIT}:${PARENT_IMAGE_TAG} remote-ubuntu-version mariadb-server | sed 's/^.*://g' | sed 's/[^0-9a-zA-Z\._-]//g')";
 overrideEnvVar TAG '${UBUNTU_VERSION}';
 defineEnvVar MARIADB_ROOT_PASSWORD MANDATORY "The password for the root user" "${RANDOM_PASSWORD}";
 defineEnvVar MARIADB_ADMIN_USER MANDATORY "The admin user" "dbadmin";
