5d173ec01324034a028e53cfea97e34d63618a43
Jose San Leandro <josesla@ext.inditex.com>
2020-04-01 22:09:07 +0200

    Changes in system-update script

--- .templates/install-files/system-update.sh
+++ .templates/install-files/system-update.sh
@@ -1,14 +1,15 @@
 #!/bin/bash dry-wit
-
 # Copyright 2015-today Automated Computing Machinery S.L.
 # Distributed under the terms of the GNU General Public License v3
+# mod: system-update
+# api: public
+# txt: Updates the system packages.
 
-## Updates the system via apt-get update
-## <- 0 if the system gets updated; 1 otherwise.
-## Example:
-##   if ! update_system; then
-##     echo "Error updating system"
-##   fi
+# fun: update_system
+# api: public
+# txt: Updates the system via apt update.
+# txt: Returns 0/TRUE if the system gets updated; 1/FALSE otherwise.
+# use: if ! update_system; then echo "Error updating system"; fi
 function update_system() {
   local -i _rescode;
 
@@ -30,18 +31,42 @@ function update_system() {
   return ${_rescode};
 }
 
+# fun: upgrade_system
+# api: public
+# txt: Updates the system via apt upgrade.
+# txt: Returns 0/TRUE if the system gets upgraded; 1/FALSE otherwise.
+# use: if ! upgrade_system; then echo "Error upgrading system"; fi
+function upgrade_system() {
+  local -i _rescode;
+
+  if isEmpty "${SYSTEM_UPGRADE}"; then
+    exitWithErrorCode SYSTEM_UPGRADE_IS_MANDATORY;
+  fi
+
+  logInfo -n "Upgrade system (this can take some time)";
+
+  ${SYSTEM_UPGRADE} > /dev/null 2>&1
+  _rescode=$?;
+
+  if isTrue ${_rescode}; then
+    logInfoResult SUCCESS "done";
+  else
+    logInfoResult FAILURE "failed";
+  fi
+
+  return ${_rescode};
+}
 
 ## Main logic
 ## dry-wit hook
 function main() {
   update_system;
+
+  upgrade_system;
 }
 
 ## Script metadata and CLI settings.
 
 setScriptDescription "Updates the system packages.";
-
-addError "INVALID_OPTION" "Unrecognized option";
-
 addError SYSTEM_UPDATE_IS_MANDATORY "SYSTEM_UPDATE is mandatory"
-#
+# vim: syntax=sh ts=2 sw=2 sts=4 sr noet
