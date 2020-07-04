e403a3c7ef78a42323709d80d1039d326a8b4545
Jose San Leandro <jose.sanleandro@osoco.es>
2020-07-04 19:55:34 +0200

    Some improvements on Postgresql image

--- .templates/postgresql-files/process-sql.sh
+++ .templates/postgresql-files/process-sql.sh
@@ -1,141 +1,275 @@
-#!/bin/bash /usr/local/bin/dry-wit
+#!/bin/bash dry-wit
 # Copyright 2014-today Automated Computing Machinery S.L.
 # Distributed under the terms of the GNU General Public License v3
+# mod: process-sql.sh
+# api: public
+# txt: Connects to a PostgreSQL server and processes the SQL files in a volume.
 
-function usage() {
-cat <<EOF
-$SCRIPT_NAME [-v[v]|-q|--quiet] [-u|--user] user [-p|--password] password [[-db|--database] database]? [[-f|--file] file]?
-$SCRIPT_NAME [-h|--help]
-(c) 2015-today Automated Computing Machinery S.L.
-    Distributed under the terms of the GNU General Public License v3
- 
-Connects to a PostgreSQL server and processes the SQL files in a volume.
+# fun: main
+# api: public
+# txt: Connects to a PostgreSQL server and processes the SQL files in a volume.
+# txt: Returns 0/TRUE always, but can exit if there're no sql files to process.
+# use: main;
+function main() {
+  local _atLeastOneFile=${FALSE};
 
-Where:
-  * -u | --user: the database user. Defaults to "${DEFAULT_DB_USER}".
-  * -p | --password: the database password.
-  * -db | --database: The database name. Defaults to "${DEFAULT_DB_NAME}".
-  * -f | --file: the input file. Optional. If not specified, all files in the mounted /sql volume will be used as input.
-Common flags:
-    * -h | --help: Display this message.
-    * -v: Increase the verbosity.
-    * -vv: Increase the verbosity further.
-    * -q | --quiet: Be silent.
-EOF
+  if isNotEmpty "${SQL_FILE}"; then
+    _atLeastOneFile=${TRUE};
+    process_sql_file "${SQL_FOLDER}/${SQL_FILE}";
+  else
+    if find_sql_files "${SQL_FOLDER}"; then
+      local _sqlFiles="${RESULT}";
+      local _oldIFS="${IFS}";
+      local _file;
+      IFS="${DWIFS}";
+      for _file in ${_sqlFiles}; do
+        IFS="${_oldIFS}";
+        if process_sql_file "${SQL_FOLDER}/${_file}"; then
+          _atLeastOneFile=${TRUE};
+        fi
+      done
+      IFS="${_oldIFS}";
+
+      if isFalse ${_atLeastOneFile}; then
+        logInfo "No pending files to process in ${SQL_FOLDER}";
+      fi
+    fi
+  fi
 }
 
-# Requirements
-function checkRequirements() {
-  checkReq awk AWK_NOT_INSTALLED;
-  checkReq envsubst ENVSUBST_NOT_INSTALLED;
-  checkReq tr TR_NOT_INSTALLED;
+# fun: process_sql_file file
+# api: public
+# txt: Processes given SQL file.
+# opt: file: The SQL file to process.
+# txt: Returns 0/TRUE if the file is processed and could be marked as such; 1/FALSE otherwise.
+# use: if process_sql_file /tmp/my.sql; then
+# use:   echo "Done";
+# use: else
+# use:   cho "Failed";
+# use: fi
+function process_sql_file() {
+  local _file="${1}";
+  checkNotEmpty file "${_file}" 1;
+
+  local _inputFile;
+
+  logInfo -n "Processing ${_file}";
+  if replace_placeholders "${_file}"; then
+    _inputFile="${RESULT}";
+  else
+    _inputFile="${_file}";
+  fi
+  createTempFile;
+  local _logFile="${RESULT}";
+
+  cat "${_logFile}";
+
+  local -i _rescode;
+  if isEmpty "${DB_PASSWORD}"; then
+    /usr/bin/psql -X -n -h ${DB_HOST} -f ${_file} -o ${_logFile} -U ${DB_USER} -w --dbname=${DB_NAME} 2>&1;
+    _rescode=$?;
+  else
+    PGPASSWORD="${DB_PASSWORD}" /usr/bin/psql -X -n -h ${DB_HOST} -f ${_file} -o ${_logFile} -U ${DB_USER} --dbname=${DB_NAME} 2>&1;
+    _rescode=$?;
+  fi
+
+  if isTrue ${_rescode}; then
+    if ! mark_sql_file_as_processed "$(dirname "${_file}")" "$(basename "${_file}")"; then
+      logInfoResult SUCCESS "warning";
+      logInfo "Cannot mark ${_file} as processed";
+      _rescode=${FALSE};
+    else
+      logInfoResult SUCCESS "done";
+    fi
+  else
+    logInfoResult FAILURE "failed";
+    export ERROR="$(cat ${_logFile})";
+  fi
+
+  return ${_rescode};
 }
 
-# Error messages
-function defineErrors() {
-  addError INVALID_OPTION "Unrecognized option";
-  addError AWK_NOT_INSTALLED "awk is not installed";
-  addError ENVSUBST_NOT_INSTALLED "envsubst is not installed";
-  addError TR_NOT_INSTALLED "tr is not installed";
-  addError NO_DB_USER_SPECIFIED "The database user cannot be empty";
-  addError NO_DB_NAME_SPECIFIED "The database name cannot be empty";
-  addError NO_SQL_FOLDER_SPECIFIED "The SQL folder is mandatory";
-  addError NO_SQL_FILES_FOUND "No SQL files found in ${SQL_FOLDER}";
-  addError SQL_FILE_NOT_FOUND "The specified file does not exist";
-  addError SQL_FILE_NOT_READABLE "The specified file is not readable";
-  addError SQL_FILE_IS_NOT_A_FILE "The specified file is not a file";
-}
+# fun: find_sql_files folder
+# api: public
+# txt: Finds the SQL files in given folder.
+# opt: The folder.
+# txt: Returns 0/TRUE if there's at least one sql file yet to process.
+# txt: If the function returns 0/TRUE, the variable RESULT contains a space-separated list of file names (relative to the folder).
+# use: local f;
+# use: local oldIFS="${IFS}";
+# use: IFS="${DWIFS}";
+# use: for f in find_sql_files /tmp; do
+# use:   IFS="${oldIFS}";
+# use:   echo "Found SQL: $f";
+# use: done;
+# use: IFS="${oldIFS}";
+function find_sql_files() {
+  local _folder="${1}";
+  checkNotEmpty folder "${_folder}" 1;
+  local _result=();
 
-## Parses the input
-## dry-wit hook
-function parseInput() {
+  local -i _rescode=${FALSE};
 
-  local _flags=$(extractFlags $@);
-  local _flagCount;
-  local _currentCount;
+  local _oldIFS="${IFS}";
+  local IFS="${DWIFS}";
+  local _aux;
 
-  # Flags
-  for _flag in ${_flags}; do
-    _flagCount=$((_flagCount+1));
-    case ${_flag} in
-      -h | --help | -v | -vv | -q)
-         shift;
-         ;;
-      -u | --user)
-         shift;
-         DB_USER="${1}";
-         shift;
-         ;;
-      -p | --password)
-         shift;
-         DB_PASSWORD="${1}";
-         shift;
-         ;;
-      -db | --database)
-         shift;
-         DB_NAME="${1}";
-         shift;
-         ;;
-      -f | --file)
-         shift;
-         SQL_FILE="${1}";
-         shift;
-         ;;
-    esac
+  for _aux in $(ls -a "${_folder}/" | grep -v '^\..*$'); do
+    IFS="${_oldIFS}";
+    if ! is_sql_file_marked_as_processed "${_folder}" "${_aux}"; then
+      _rescode=${TRUE};
+      _result[${#_result[@]}]="${_aux}";
+    fi
   done
+  IFS="${_oldIFS}";
 
-  if [[ -z ${DB_USER} ]]; then
-    DB_USER="${DEFAULT_DB_USER}";
-  fi
+  export RESULT=${_result[@]};
 
-  if [[ -z ${DB_NAME} ]]; then
-    DB_NAME="${DEFAULT_DB_NAME}";
-  fi
-
-  # Parameters
-  if [[ -z ${SQL_FOLDER} ]]; then
-    SQL_FOLDER="$1";
-    shift;
-  fi
+  return ${TRUE};
 }
 
-## Checking input
-## dry-wit hook
-function checkInput() {
+# fun: is_sql_file_marked_as_processed folder file
+# api: public
+# txt: Checks whether given SQL file is marked as processed.
+# opt: folder: The SQL folder.
+# opt: file: The SQL file.
+# txt: Returns 0/TRUE if it's marked as processed; 1/FALSE otherwise.
+# use: if is_sql_file_marked_as_processed /tmp my.sql; then
+# use:   echo "/tmp/my.sql Already processed";
+# use: fi
+function is_sql_file_marked_as_processed() {
+  local _folder="${1}";
+  checkNotEmpty folder "${_folder}" 1;
+  local _file="${2}";
+  checkNotEmpty file "${_file}" 2;
 
-  local _flags=$(extractFlags $@);
-  local _flagCount;
-  local _currentCount;
-  logDebug -n "Checking input";
+  local -i _rescode=${TRUE};
 
-  # Flags
-  for _flag in ${_flags}; do
-    _flagCount=$((_flagCount+1));
-    case ${_flag} in
-      -h | --help | -v | -vv | -q | --quiet | -u | --user | -p | --password | -db | --database | -f | --file)
-         ;;
-      *) logDebugResult FAILURE "fail";
-         exitWithErrorCode INVALID_OPTION ${_flag};
-         ;;
-    esac
-  done
+  if fileExists "${_folder}"/."${_file}".done; then
+    _rescode=${TRUE};
+  else
+    _rescode=${FALSE};
+  fi
 
-  if [[ -z ${DB_USER} ]]; then
-    logDebugResult FAILURE "fail";
+  return ${_rescode};
+}
+
+# fun: mark_sql_file_as_processed folder file
+# api: public
+# txt: Marks given SQL file as processed.
+# opt: folder: The SQL folder.
+# opt: file: The SQL file.
+# txt: Returns 0/TRUE if it's marked successfully; 1/FALSE otherwise.
+# use: if ! mark_sql_file_as_processed /tmp my.sql; then
+# use:   echo "Cannot mark SQL file as processed";
+# use: fi
+function mark_sql_file_as_processed() {
+  local _folder="${1}";
+  checkNotEmpty folder "${_folder}" 1;
+  local _file="${2}";
+  checkNotEmpty file "${_file}" 2;
+
+  touch "${_folder}"/."${_file}".done 2> /dev/null;
+
+  return $?;
+}
+
+# fun: replace_placeholders file
+# api: public
+# txt: Replaces any placeholders in given file, using the existing environment variables.
+# opt: file: The SQL file to process.
+# txt: Returns 0/TRUE if the file is processed, 1/FALSE otherwise.
+# txt: If the function returns 0/TRUE, the variable RESULT contains the path of the processed file.
+# use: if replace_placeholders /var/local/my-file.template; then
+# use:   echo "my-file.template processed: ${RESULT}";
+# use: fi
+function replace_placeholders() {
+  local _file="${1}";
+  checkNotEmpty file "${_file}" 1;
+
+  createTempFile;
+  local _output="${RESULT}";
+
+  local _env="$(IFS=" \t" env | grep -v -e '^_DW_' | grep -v -e '^DW_' | awk -F'=' '{if ($1!="") { printf("%s=\"%s\" ", $1, $2);}}')";
+
+  local _envsubstDecl=$(echo -n "'"; IFS=" \t" env | grep -v -e '^_DW_' | grep -v -e '^DW_' | cut -d'=' -f 1 | grep -v -e '^$' | awk '{ printf("${%s} ", $0);}'; echo -n "'";);
+
+  echo "${_env} envsubst ${_envsubstDecl} < ${_file} > ${_output}" | sh;
+  local -i _rescode=$?;
+
+  if isTrue ${_rescode}; then
+    export RESULT="${_output}";
+  fi
+
+  return ${_rescode};
+}
+
+# Script metadata
+setScriptDescription "Connects to a PostgreSQL server and processes the SQL files in a volume.";
+
+checkReq awk AWK_NOT_INSTALLED;
+checkReq envsubst ENVSUBST_NOT_INSTALLED;
+checkReq tr TR_NOT_INSTALLED;
+
+addError NO_DB_USER_SPECIFIED "The database user cannot be empty";
+addError NO_DB_NAME_SPECIFIED "The database name cannot be empty";
+addError NO_SQL_FOLDER_SPECIFIED "The SQL folder is mandatory";
+addError NO_SQL_FILES_FOUND "No SQL files found in ${SQL_FOLDER}";
+addError SQL_FILE_NOT_FOUND "The specified file does not exist";
+addError SQL_FILE_NOT_READABLE "The specified file is not readable";
+addError SQL_FILE_IS_NOT_A_FILE "The specified file is not a file";
+
+addCommandLineFlag user u "The database user" OPTIONAL EXPECTS_ARGUMENT "${DEFAULT_DB_USER}";
+function dw_check_user_cli_flag() {
+  local _user="${1}";
+  if isEmpty "${_user}"; then
     exitWithErrorCode NO_DB_USER_SPECIFIED;
   fi
+}
+function dw_parse_user_cli_flag() {
+  export DB_USER="${1}";
+}
 
-  if [[ -z ${DB_NAME} ]]; then
-    logDebugResult FAILURE "fail";
+addCommandLineFlag password p "The password of the database user" OPTIONAL EXPECTS_ARGUMENT;
+function dw_parse_password_cli_flag() {
+  export DB_PASSWORD="${1}";
+}
+
+addCommandLineFlag database db "The database name" OPTIONAL EXPECTS_ARGUMENT "${DEFAULT_DB_NAME}";
+function dw_check_database_cli_flag() {
+  local _name="${1}";
+  if isEmpty "${_name}"; then
     exitWithErrorCode NO_DB_NAME_SPECIFIED;
   fi
+}
+function dw_parse_database_cli_flag() {
+  export DB_NAME="${1}";
+}
+addCommandLineFlag file f "The sql file" OPTIONAL EXPECTS_ARGUMENT;
+function dw_parse_file_cli_flag() {
+  export SQL_FILE="${1}";
+}
+addCommandLineParameter sqlFolder "The folder with sql files to process" OPTIONAL SINGLE;
 
-  if [[ -z ${SQL_FOLDER} ]]; then
-    if [[ -z ${SQL_FILE} ]]; then
+defineEnvVar DB_HOST MANDATORY "The PostgreSQL host" "localhost";
+defineEnvVar DEFAULT_DB_USER MANDATORY "The default database user" "${POSTGRESQL_ROOT_USER}";
+defineEnvVar DEFAULT_DB_NAME MANDATORY "The default database name" "postgres";
+
+# fun: checkInput
+# api: public
+# txt: Checks input parameters and flags.
+# txt: Returns 0/TRUE always, but can exit if the input parameters are invalid.
+# use: checkInput;
+function checkInput() {
+
+  logDebug -n "Checking input";
+
+  if isEmpty "${SQL_FOLDER}"; then
+    if isEmpty "${SQL_FILE}"; then
       logDebugResult FAILURE "fail";
       exitWithErrorCode NO_SQL_FOLDER_SPECIFIED;
     else
-      if [[ -e ${SQL_FILE} ]]; then
+      if fileExists "${SQL_FILE}"; then
         if [[ -r ${SQL_FILE} ]]; then
           if [[ -f ${SQL_FILE} ]]; then
             logDebugResult SUCCESS "valid";
@@ -157,126 +291,4 @@ function checkInput() {
   fi
 }
 
-## Checks whether given SQL file is marked as processed.
-## -> 1: The SQL folder.
-## -> 2: The SQL file.
-## <- 0 if it's marked as processed already, 1 otherwise.
-## Example:
-##   if is_sql_file_marked_as_processed /tmp my.sql; then echo "/tmp/my.sql Already processed" ; fi
-function is_sql_file_marked_as_processed() {
-  local _folder="${1}";
-  local _file="${2}";
-  local _rescode=0;
-  if [ -e "${_folder}"/."${_file}".done ]; then
-    _rescode=0;
-  else
-    _rescode=1;
-  fi
-  return $?;
-}
-
-## Marks given SQL file as processed.
-## -> 1: The SQL folder.
-## -> 2: The SQL file.
-## <- 0 if it's marked successfully, 1 otherwise.
-## Example:
-##   if ! mark_sql_file_as_processed /tmp my.sql; then echo "Cannot mark SQL file as processed"; fi
-function mark_sql_file_as_processed() {
-  local _folder="${1}";
-  local _file="${2}";
-  touch "${_folder}"/."${_file}".done 2> /dev/null
-  return $?;
-}
-
-## Finds the SQL files in given folder.
-## -> 1: The folder
-## <- RESULT: A space-separated list of file names (relative to the folder).
-## Example:
-##   for f in find_sql_files /tmp; do echo "Found SQL: $f"; done
-function find_sql_files() {
-  local _folder="${1}";
-  local _result=();
-
-  for _aux in $(ls -a "${_folder}" | grep -v '^\..*$'); do
-    if is_sql_file_marked_as_processed "${_folder}" "${_aux}"; then
-      _result[${#_result[@]}]="${_aux}";
-    fi
-  done
-  export RESULT=${_result[@]};
-}
-
-## Replaces any placeholders in given file.
-## -> 1: The SQL file to process.
-## <- 0 if the file is processed, 1 otherwise.
-## <- RESULT: the path of the processed file.
-function replace_placeholders() {
-  local _file="${1}";
-  local _rescode;
-  createTempFile;
-  local _output="${RESULT}";
-  local _env="$(IFS=" \t" env | awk -F'=' '{printf("%s=\"%s\" ", $1, $2);}')";
-  local _envsubstDecl=$(echo -n "'"; IFS=" \t" env | cut -d'=' -f 1 | awk '{printf("${%s} ", $0);}'; echo -n "'";);
-
-  echo "${_env} envsubst ${_envsubstDecl} < ${_file} > ${_output}" | sh;
-  _rescode=$?;
-  export RESULT="${_output}";
-  return ${_rescode};
-}
-
-## Processes given SQL file.
-## -> 1: The SQL file to process.
-## <- 0 if the file is processed, 1 otherwise.
-## Example:
-##   if process_sql_file /tmp/my.sql; then echo "Done"; else echo "Failed"; fi
-function process_sql_file() {
-  local _file="${1}";
-  local _inputFile;
-  local _logFile;
-  logInfo -n "Processing ${_file}";
-  if replace_placeholders "${_file}"; then
-    _inputFile="${RESULT}";
-  else
-    _inputFile="${_file}";
-  fi
-  createTempFile;
-  _logFile="${RESULT}";
-  if [[ -z ${DB_PASSWORD} ]]; then
-    /usr/bin/psql -X -n -h ${DB_HOST} -f ${_file} -o ${_logFile} -U ${DB_USER} -w --dbname=${DB_NAME} 2>&1
-  else
-    PGPASSWORD="${DB_PASSWORD}" /usr/bin/psql -X -n -h ${DB_HOST} -f ${_file} -o ${_logFile} -U ${DB_USER} --dbname=${DB_NAME} 2>&1
-  fi
-  if [ $? -eq 0 ]; then
-    if ! mark_sql_file_as_processed "$(dirname "${_file}")" "$(basename "${_file}")"; then
-      logInfoResult SUCCESS "warning";
-      logInfo "Cannot mark ${_file} as processed";
-    else
-      logInfoResult SUCCESS "done";
-    fi
-  else
-    logInfoResult FAILURE "failed";
-    cat ${_logFile};
-  fi
-}
-
-## Main logic
-## dry-wit hook
-function main() {
-  local _sqlFiles;
-  local _atLeastOneFile=${FALSE};
-
-  if [[ -n ${SQL_FILE} ]]; then
-    _atLeastOneFile=${TRUE};
-    process_sql_file "${SQL_FOLDER}/${SQL_FILE}";
-  else
-    find_sql_files "${SQL_FOLDER}";
-    _sqlFiles="${RESULT}";
-    for _file in ${_sqlFiles}; do
-      _atLeastOneFile=${TRUE};
-      process_sql_file "${SQL_FOLDER}/${_file}";
-    done
-    if [ ${_atLeastOneFile} -ne ${TRUE} ]; then
-      exitWithErrorCode NO_SQL_FILES_FOUND;
-    fi
-  fi
-}
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
--- postgresql-bootstrap/build-settings.sh
+++ postgresql-bootstrap/build-settings.sh
@@ -1,2 +1,3 @@
-defineEnvVar PARENT_IMAGE_TAG MANDATORY "The version of the parent image" "${TAG}";
+defineEnvVar PARENT_IMAGE_TAG MANDATORY "The version of the parent image" "0.11";
+overrideEnvVar TAG '9.6';
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
