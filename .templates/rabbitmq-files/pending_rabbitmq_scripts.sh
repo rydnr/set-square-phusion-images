7441cedb9ddbce755a5febb24db50bd04cdc3c13
Jose San Leandro <jose.sanleandro@osoco.es>
2020-07-04 19:53:42 +0200

    RabbitMQ can now accept bootstrap scripts while it's running

---
+++ .templates/rabbitmq-files/pending_rabbitmq_scripts.sh
@@ -0,0 +1,134 @@
+#!/bin/bash dry-wit
+# Copyright 2017-today OSOCO
+# mod: pending_bootstrap_scripts.sh
+# api: public
+# txt: Detects pending RabbitMQ scripts, and runs them.
+
+DW.import rabbitmq;
+
+# fun: main
+# api: public
+# txt: Detects pending RabbitMQ scripts, and runs them.
+# txt: Returns 0/TRUE always, but can exit in case of error.
+# use: main
+function main() {
+  if find_pending_scripts; then
+    local _scripts="${RESULT}";
+
+    local _oldIFS="${IFS}";
+    IFS="${DWIFS}";
+    local _script;
+    for _script in ${_scripts}; do
+      if run_script "${_script}"; then
+        mark_script_as_done "${_script}";
+      fi
+    done;
+    IFS="${_oldIFS}";
+  fi
+
+  rabbitmqMigrate;
+}
+
+# fun: find_pending_scripts
+# api: public
+# txt: Retrieves the pending scripts.
+# txt: Returns 0/TRUE if there're any script still pending; 1/FALSE otherwise.
+# txt: If the function returns 0/TRUE, the variable RESULT contains the pending scripts.
+# use: if find_pending_scripts; then
+# use:   echo "Pending scripts: ${RESULT}";
+# use: fi
+function find_pending_scripts() {
+  local _candidates="$(find "${PENDING_SCRIPTS_FOLDER}" --maxdepth 1)";
+
+  local _result="";
+  local -i _rescode=${FALSE};
+
+  local _oldIFS="${IFS}";
+  local _candidate;
+  IFS="${DWIFS}";
+  for _candidate in ${_candidates}; do
+    if ! already_done "${_candidate}"; then
+      _rescode=${TRUE};
+      if isNotEmpty "${_result}"; then
+        _result="${_result} ";
+      fi
+      _result="${_result}${_candidate}";
+    fi
+  done;
+
+  if isTrue ${_rescode}; then
+    export RESULT="${_result}";
+  fi
+
+  return ${_rescode};
+}
+
+# fun: already_done script.
+# api: public
+# txt: Checks whether given script is already done or not.
+# opt: script: The script.
+# txt: Returns 0/TRUE if the script is already done; 1/FALSE otherwise.
+# use: if already_done /var/local/src/rabbitmq-pending/00-myuser.sh; then
+# use:   echo "00-myuser.sh already applied";
+# use: fi
+function already_done() {
+  local _script="${1}";
+  checkNotEmpty script "${_script}" 1;
+
+  local -i _rescode=${FALSE};
+
+  local _basename="$(basename "${_script}")";
+  if fileExists "${DONE_SCRIPTS_FOLDER}/${_basename}.done"; then
+    _rescode=${TRUE};
+  fi
+
+  return ${_rescode};
+}
+
+# fun: mark_script_as_done script
+# api: public
+# txt: Marks given script as done.
+# opt: script: The script.
+# txt: Returns 0/TRUE if the script is marked as done successfully; 1/FALSE otherwise.
+# use: if mark_script_as_done /var/local/src/rabbitmq-pending/00-myuser.sh; then
+# use:   echo "00-myuser.sh annotated as done";
+# use: fi
+function mark_script_as_done() {
+  local _script="${1}";
+  checkNotEmpty script "${_script}" 1;
+
+  local -i _rescode=${FALSE};
+
+  local _basename="$(basename "${_script}")";
+  touch "${DONE_SCRIPTS_FOLDER}${_basename}.done" 2> /dev/null;
+
+  if fileExists "${DONE_SCRIPTS_FOLDER}/${_basename}.done"; then
+    _rescode=${TRUE};
+  fi
+
+  return ${_rescode};
+}
+
+# fun: run_script script
+# api: public
+# txt: Runs given script.
+# opt: script: The script to run.
+# txt: Returns the return code of the script itself.
+# use: if run_script /var/local/src/rabbitmq-pending/00-myuser.sh; then
+# use:   echo "00-myuser.sh returned 0";
+# use: fi
+function run_script() {
+  local _script="${1}";
+  checkNotEmpty script "${_script}" 1;
+
+  "${_script}";
+}
+
+# script metadata
+setScriptDescription "Detects pending RabbitMQ scripts, and runs them.";
+
+# env: PENDING_SCRIPTS_FOLDER: The folder with the pending scripts.
+defineEnvVar PENDING_SCRIPTS_FOLDER MANDATORY "The folder with the pending scripts" "/var/local/src/rabbitmq-pending";
+# env: DONE_SCRIPTS_FOLDER: The folder with the scripts already executed.
+defineEnvVar DONE_SCRIPTS_FOLDER MANDATORY "The folder with the scripts already executed" "/var/local/src/rabbitmq-done";
+# vim: syntax=sh ts=2 sw=2 sts=4 sr noet
