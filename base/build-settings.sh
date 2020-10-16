c0de81911b9a7e5570278764a897bfe5fe9d6d01
Jose San Leandro <jose.sanleandro@osoco.es>
2020-10-16 07:37:04 +0200

    Updated base to 10.04-1.0.0-amd64

--- base/build-settings.sh
+++ base/build-settings.sh
@@ -1,4 +1,4 @@
-defineEnvVar PARENT_IMAGE_TAG MANDATORY "The parent image tag" "0.11";
+defineEnvVar PARENT_IMAGE_TAG MANDATORY "The parent image tag" "18.04-1.0.0-amd64";
 defineEnvVar TAG MANDATORY "The tag" "${PARENT_IMAGE_TAG}";
 defineEnvVar DISABLE_ANSI_COLORS MANDATORY "Whether to disable ANSI colors" false;
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
