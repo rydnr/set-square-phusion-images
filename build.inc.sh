619086d10582a55c7e9cee304653ebcb472dcca3
Jose San Leandro <jose.sanleandro@osoco.es>
2020-10-20 00:25:34 +0200

    Updated set-square. Minor fixes

--- build.inc.sh
+++ build.inc.sh
@@ -1,6 +1,14 @@
-defineEnvVar REGISTRY MANDATORY "cloud.docker.com";
-overrideEnvVar LICENSE_FILE "LICENSE.gpl3";
-overrideEnvVar COPYRIGHT_PREAMBLE_FILE "copyright-preamble.gpl3";
+defineEnvVar PARENT_IMAGE_TAG OPTIONAL "The tag of the parent image";
+defineEnvVar NAMESPACE OPTIONAL "The namespace";
+defineEnvVar REGISTRY MANDATORY "The domain name of the Docker registry to use" "cloud.docker.com";
+defineEnvVar REGISTRY_NAMESPACE OPTIONAL "The namespace to use in the remote registry" "${NAMESPACE}";
+defineEnvVar DOMAIN MANDATORY "The domain name for the images" "${NAMESPACE}.com";
+defineEnvVar AUTHOR MANDATORY "The author/maintainer of the image" "${USER}";
+defineEnvVar AUTHOR_EMAIL MANDATORY "The email of the author/maintainer" "${user}@${DOMAIN}";
+defineEnvVar GIT_USER_NAME MANDATORY "The git user name" "${AUTHOR}";
+defineEnvVar GIT_USER_EMAIL MANDATORY "The git user email" "${AUTHOR_EMAIL}";
+defineEnvVar LICENSE_FILE MANDATORY "The file containing details regarding the license applied" "LICENSE.gpl3";
+defineEnvVar COPYRIGHT_PREAMBLE_FILE MANDATORY "The file contaning the copyright preamble to use" "copyright-preamble.gpl3";
 defineEnvVar DATE MANDATORY "The date format used to tag images" "$(date '+%Y%m')";
 defineEnvVar TIME MANDATORY "A timestamp" "$(date)";
 defineEnvVar RANDOM_PASSWORD MANDATORY "A random password" "$(head -c 20 /dev/urandom | sha1sum | cut -d' ' -f1)";
@@ -8,8 +16,9 @@ defineEnvVar PUSH_TO_DOCKERHUB MANDATORY "Whether to push to Docker HUB" 'false'
 defineEnvVar BUILDER MANDATORY "The builder of the image" '${AUTHOR}';
 defineEnvVar SETSQUARE_FLAVOR MANDATORY "The flavor of set-square" "phusion";
 defineEnvVar INCLUDES_FOLDER MANDATORY "The folder where 'include' files are located" "./.templates";
-defineEnvVar ROOT_IMAGE_VERSION MANDATORY "The root image version" "0.11";
-defineEnvVar ROOT_IMAGE MANDATORY "The default root image" "phusion/baseimage"
+defineEnvVar ROOT_IMAGE_VERSION MANDATORY "The root image version" "18.04-1.0.0-amd64";
+defineEnvVar TAG MANDATORY "The image tag" '${ROOT_IMAGE_VERSION}';
+defineEnvVar ROOT_IMAGE MANDATORY "The default root image" "phusion/baseimage";
 defineEnvVar ROOT_IMAGE_64BIT MANDATORY "The default root image for 64 bits" '${ROOT_IMAGE}';
 defineEnvVar ROOT_IMAGE_32BIT MANDATORY "The default root image for 32 bits" "${ROOT_IMAGE_64BIT_DEFAULT}32";
 defineEnvVar CUSTOM_NAMESPACE MANDATORY "The custom namespace" '${NAMESPACE}-${SETSQUARE_FLAVOR}';
@@ -86,7 +95,7 @@ defineEnvVar ENABLE_LOCAL_SMTP MANDATORY "Whether to run a local SMTP server" "t
 defineEnvVar ENABLE_LOGSTASH \
              MANDATORY \
              "Whether to enable logstash, if available for the specific image" \
-             "true";
+             false;
 defineEnvVar BUILDER MANDATORY "The builder of the image" '${AUTHOR}';
 defineEnvVar SSL_KEY_ENCRYPTION MANDATORY "The encryption of the key" "des3";
 defineEnvVar SSL_KEY_ALGORITHM MANDATORY "The algorithm of the SSL key" "rsa";
@@ -96,7 +105,6 @@ defineEnvVar SSL_KEY_PASSWORD MANDATORY "The key password" 'K8emqG04hZKlOUY3rET1
 defineEnvVar SSL_CERTIFICATE_ORGANIZATIONAL_UNIT MANDATORY "The organizational unit for the SSL certificate" "IT";
 defineEnvVar SSL_CERTIFICATE_ORGANIZATION MANDATORY "The organization behind the SSL certificate" '${DOMAIN}';
 defineEnvVar SSL_CERTIFICATE_ALIAS MANDATORY "The certificate alias" '${IMAGE}';
-
 defineEnvVar SSL_CERTIFICATE_STATE MANDATORY "The state information in the SSL certificate" "Madrid";
 defineEnvVar SSL_CERTIFICATE_COUNTRY MANDATORY "The country information in the SSL certificate" "ES";
 defineEnvVar SSL_CERTIFICATE_EXPIRATION_DAYS MANDATORY "The number of days until the certificate expires" "3650";
@@ -106,4 +114,11 @@ defineEnvVar SSL_KEYSTORE_TYPE MANDATORY "The type of the keystore" "jks";
 defineEnvVar SSL_JAVA_SIGN_ALGORITHM MANDATORY "The algorithm used to sign the SSL certificate" "SHA256withRSA";
 defineEnvVar HOST_VOLUMES_ROOT_FOLDER MANDATORY "The root folder for host volumes" "/var/lib/docker/data";
 defineEnvVar DEVELOPMENT_USER_ID MANDATORY "The user id used when developing code (to match host user id)" "$(id -u)";
+defineEnvVar BACKUP_HOST MANDATORY "The name of the backup host" "backup.${DOMAIN}";
+defineEnvVar DOCKER_TAG_OPTS OPTIONAL "Custom options when tagging images";
+defineEnvVar DOCKER_BUILD_OPTS OPTIONAL "Custom options when building images";
+defineEnvVar SERVICE_USER OPTIONAL "The service user";
+defineEnvVar SERVICE_GROUP OPTIONAL "The service group";
+defineEnvVar SERVICE_USER_HOME OPTIONAL "The home of the service user";
+defineEnvVar SERVICE_USER_SHELL OPTIONAL "The shell of the service user";
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
