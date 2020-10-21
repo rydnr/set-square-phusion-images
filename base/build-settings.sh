8dbbe50eb19e413867174efe827ec5217c1e65d0
Jose San Leandro <jose.sanleandro@osoco.es>
2020-10-21 11:29:59 +0200

    Updated tag

--- base/build-settings.sh
+++ base/build-settings.sh
@@ -1,7 +1,7 @@
 # env: PARENT_IMAGE_TAG: The tag of the parent image. Defaults to 18.04-1.0.0-amd64.
-defineEnvVar PARENT_IMAGE_TAG MANDATORY "The tag of the parent image" "18.04-1.0.0-amd64";
+overrideEnvVar PARENT_IMAGE_TAG "18.04-1.0.0-amd64";
 # env: TAG: The tag of the image. Defaults to ${PARENT_IMAGE_TAG}.
-overrideEnvVar TAG "The tag" "${PARENT_IMAGE_TAG}";
+overrideEnvVar TAG "${PARENT_IMAGE_TAG}";
 # env: DISABLE_ANSI_COLORS: Whether to disable ANSI colors. Defaults to true.
 defineEnvVar DISABLE_ANSI_COLORS MANDATORY "Whether to disable ANSI colors" true;
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
--- nodejs/build-settings.sh
+++ nodejs/build-settings.sh
@@ -1,10 +1,6 @@
-defineEnvVar PARENT_IMAGE_TAG MANDATORY "The base tag" "0.11";
 defineEnvVar NODEJS_MAJOR_VERSION MANDATORY "The major version of NodeJS" "8";
 overrideEnvVar TAG '${NODEJS_MAJOR_VERSION}';
-defineEnvVar SERVICE_USER MANDATORY "The name of the user" '${NAMESPACE}';
-defineEnvVar SERVICE_GROUP MANDATORY "The name of the group" "users";
-defineEnvVar SERVICE_USER_HOME MANDATORY "The home of the nodejs user" '/home/${SERVICE_USER}';
-defineEnvVar SERVICE_USER_SHELL MANDATORY "The shell of the nodejs user" "/bin/bash";
+overrideEnvVar SERVICE_USER '${NAMESPACE}';
 overrideEnvVar ENABLE_RSNAPSHOT "false";
 overrideEnvVar ENABLE_LOGSTASH "false";
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
