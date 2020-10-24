1fc0afca42e59413f4ecb45b40d1af7dc1562c8c
Jose San Leandro <jose.sanleandro@osoco.es>
2020-10-24 20:39:38 +0200

    Some work on the robo3t image

--- robo3t/build-settings.sh
+++ robo3t/build-settings.sh
@@ -1,7 +1,7 @@
-defineEnvVar PARENT_IMAGE_TAG MANDATORY "The tag of the parent image" "0.11";
-defineEnvVar ROBO3T_VERSION MANDATORY "The Robo3T version" '1.2.1';
-defineEnvVar ROBO3T_HASH MANDATORY "The Robo3T hash" '3e50a65';
-defineEnvVar TAG MANDATORY "The image tag" '${ROBO3T_VERSION}-${ROBO3T_HASH}';
+defineEnvVar PARENT_IMAGE_TAG MANDATORY "The tag of the parent image" "18.04-1.0.0-amd64";
+#defineEnvVar ROBO3T_VERSION MANDATORY "The Robo3T version" '1.2.1';
+#defineEnvVar ROBO3T_HASH MANDATORY "The Robo3T hash" '3e50a65';
+defineEnvVar TAG MANDATORY "The image tag" '1.4';
 defineEnvVar SERVICE_USER MANDATORY "The service user" 'robo3t';
 defineEnvVar SERVICE_GROUP MANDATORY "The service group" 'robo3t';
 defineEnvVar SERVICE_USER_SHELL MANDATORY "The shell of the service user" '/bin/bash';
