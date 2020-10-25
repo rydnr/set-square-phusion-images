c61e4b3613a81bda0c7e99e251912b3ca7b7c4cc
Jose San Leandro <jose.sanleandro@osoco.es>
2020-10-25 20:37:53 +0100

    Fixing build-settings.sh in gtoolkit

--- gtoolkit/build-settings.sh
+++ gtoolkit/build-settings.sh
@@ -1,11 +1,10 @@
-defineEnvVar PARENT_IMAGE_TAG MANDATORY "The tag of the parent image" "0.11";
+defineEnvVar PARENT_IMAGE_TAG MANDATORY "The tag of the parent image" "";
 defineEnvVar TAG MANDATORY "The tag" 'latest';
-overrideEnvVar ENABLE_LOGSTASH 'false';
-defineEnvVar SERVICE_USER MANDATORY "The service user" "pharo";
-defineEnvVar SERVICE_USER_PASSWORD MANDATORY "The service user password" '${RANDOM_PASSWORD}';
-defineEnvVar SERVICE_GROUP MANDATORY "The service group" "pharo";
-defineEnvVar SERVICE_USER_HOME MANDATORY "The home of the service user" "/home/pharo";
-defineEnvVar SERVICE_USER_SHELL MANDATORY "The shell of the service user" "/bin/bash";
+overrideEnvVar ENABLE_LOGSTASH false;
+overrideEnvVar SERVICE_USER MANDATORY "The service user" "pharo";
+overrideEnvVar SERVICE_USER_PASSWORD MANDATORY "The service user password" '${RANDOM_PASSWORD}';
+overrideEnvVar SERVICE_GROUP MANDATORY "The service group" "pharo";
+overrideEnvVar SERVICE_USER_HOME MANDATORY "The home of the service user" "/home/pharo";
 defineEnvVar WORKSPACE MANDATORY "The workspace folder" '${SERVICE_USER_HOME}/work';
 overrideEnvVar ENABLE_SSH "false";
 overrideEnvVar ENABLE_MONIT "false";
