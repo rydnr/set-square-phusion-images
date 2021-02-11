6fe6f30d3273a5ff5d95ecbfb7572a87a707c5b8
Jose San Leandro <jose.sanleandro@osoco.es>
2021-02-11 11:19:46 +0100

    Using focal as base image

--- base/build-settings.sh
+++ base/build-settings.sh
@@ -1,5 +1,5 @@
 # env: PARENT_IMAGE_TAG: The tag of the parent image. Defaults to master.
-overrideEnvVar PARENT_IMAGE_TAG "master";
+overrideEnvVar PARENT_IMAGE_TAG "focal-1.0.0alpha1-amd64";
 # env: TAG: The tag of the image. Defaults to ${PARENT_IMAGE_TAG}.
 overrideEnvVar TAG "${PARENT_IMAGE_TAG}";
 # env: DISABLE_ANSI_COLORS: Whether to disable ANSI colors. Defaults to true.
