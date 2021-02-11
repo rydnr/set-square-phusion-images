5eacdb6767f653b4578f1a8efbe06d7ff10c5921
Jose San Leandro <jose.sanleandro@osoco.es>
2021-02-11 11:20:09 +0100

    Using focal as base image

--- postgresql-bootstrap/build-settings.sh
+++ postgresql-bootstrap/build-settings.sh
@@ -1,3 +1,3 @@
-defineEnvVar PARENT_IMAGE_TAG MANDATORY "The version of the parent image" "0.11";
+defineEnvVar PARENT_IMAGE_TAG MANDATORY "The version of the parent image" "focal-1.0.0alpha1-amd64";
 overrideEnvVar TAG '9.6';
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
