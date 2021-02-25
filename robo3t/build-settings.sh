de543233b465e92c8df097842bfb3d00d7efff83
Jose San Leandro <jose.sanleandro@osoco.es>
2021-02-25 16:06:49 +0100

    Minor fix

--- robo3t/build-settings.sh
+++ robo3t/build-settings.sh
@@ -1,4 +1,3 @@
-defineEnvVar PARENT_IMAGE_TAG MANDATORY "The tag of the parent image" "18.04-1.0.0-amd64";
 #defineEnvVar ROBO3T_VERSION MANDATORY "The Robo3T version" '1.2.1';
 #defineEnvVar ROBO3T_HASH MANDATORY "The Robo3T hash" '3e50a65';
 defineEnvVar TAG MANDATORY "The image tag" '1.4';
