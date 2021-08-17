f6af173675c4e1c9ea79d3d6551b2e6be6572a10
Jose San Leandro <jose.sanleandro@osoco.es>
2021-08-17 19:40:36 +0200

    Improved mongodb-dump.sh script

--- .templates/mongodb-files/mongodb-dump.sh
+++ .templates/mongodb-files/mongodb-dump.sh
@@ -17,34 +17,69 @@ DW.import mongodb
 # txt: Returns 0/TRUE always.
 # use: main;
 function main() {
-	local _outputFolder="${MONGODB_DUMP_FOLDER}"
-	local _dumpFile
-	_dumpFile="${_outputFolder}/dump-$(date '+%Y%m%d.%H%M').gz"
+  echo "in main"
+  local _outputFolder="${MONGODB_DUMP_FOLDER}"
+  DW.getScriptName
+  local _scriptName
+  _scriptName="$(basename ${RESULT})"
 
-	DW.getScriptName
-	local _scriptName
-	_scriptName="$(basename ${RESULT})"
+  exec > >(sudo tee "${MONGODB_LOG_FOLDER}"/"${_scriptName}".log | sudo sh -c "logger -t \"${_scriptName}\" -s 2>/dev/console") 2>&1
 
-	exec > >(sudo tee "${MONGODB_LOG_FOLDER}"/"${_scriptName}".log | sudo sh -c "logger -t \"${_scriptName}\" -s 2>/dev/console") 2>&1
+  mkdir -p "${_outputFolder}" >/dev/null
 
-	mkdir -p "${_outputFolder}" >/dev/null
+  local _timestamp="$(date '+%Y%m%d.%H%M')"
 
-	logInfo -n "Dumping MongoDB database on localhost to ${_dumpFile}"
+  local _db
+  local _oldIFS="${IFS}"
+  IFS="${DWIFS}"
+  for _db in ${MONGODB_DBS}; do
+    IFS="${_oldIFS}"
+    local _dumpFolder
+    _dumpFolder="${_outputFolder}/${_db}-${_timestamp}"
 
-	if mongoDump "${_dumpFile}" "${MONGODB_HOST}" "${MONGODB_USER}" "${MONGODB_PASSWORD}" "${AUTHENTICATION_DATABASE}" "${AUTHENTICATION_MECHANISM}"; then
-		logInfoResult SUCCESS "done"
-	else
-		logInfoResult FAILURE "failed"
-		logInfo "${ERROR}"
-	fi
+    logInfo -n "Dumping MongoDB ${_db} database on localhost to ${_dumpFolder}"
+    if mongoDump "${_dumpFolder}" "${_db}" "${MONGODB_URL}" "${MONGODB_USER}" "${MONGODB_PASSWORD}" "${AUTHENTICATION_DATABASE}" "${AUTHENTICATION_MECHANISM}"; then
+      logInfoResult SUCCESS "done"
+    else
+      logInfoResult FAILURE "failed"
+      logInfo "${ERROR}"
+    fi
+  done
+
+  IFS="${DWIFS}"
+  for _db in ${MONGODB_DBS}; do
+    IFS="${_oldIFS}"
+    local _dumpFolder
+    _dumpFolder="${_outputFolder}/${_db}-${_timestamp}"
+    local _dumpFile
+    _dumpFile="${_dumpFolder}.gz"
+
+    logInfo -n "Creating ${_dumpFile} with the contents of folder ${_dumpFolder}"
+    if compress "${_dumpFile}" "${_dumpFolder}"; then
+      logInfoResult SUCCESS "done"
+    else
+      logInfoResult FAILURE "failed"
+      logInfo "${ERROR}"
+    fi
+    logInfo -n "Removing ${_dumpFolder}"
+    if rm -rf "${_dumpFolder}" >/dev/null 2>&1; then
+      logInfoResult SUCCESS "done"
+    else
+      logInfoResult FAILURE "failed"
+      logInfo "${ERROR}"
+    fi
+  done
+  IFS="${_oldIFS}"
 }
 
 setScriptDescription "Performs dumps of the MongoDB database."
 
 # env: MONGODB_DUMP_FOLDER: The folder storing the generated dump file. Defaults to /backup/mongodb/dumps.
 defineEnvVar MONGODB_DUMP_FOLDER OPTIONAL "The folder storing the generated dump file" "/backup/mongodb/dumps"
-# env: MONGODB_HOST: The MongoDB host. Defaults to localhost.
-defineEnvVar MONGODB_HOST OPTIONAL "The MongoDB host" "localhost"
+# env: MONGODB_DBS: The MongoDB databases to dump.
+defineEnvVar MONGODB_DBS MANDATORY "The MongoDB databases to dump"
+# env: MONGODB_URL: The MongoDB url. Defaults to mongodb://localhost.
+defineEnvVar MONGODB_URL OPTIONAL "The MongoDB url" "mongodb://localhost"
 # env: MONGODB_USER: The MongoDB user. Defaults to ${BACKUP_USER_NAME}.
 defineEnvVar MONGODB_USER OPTIONAL "The MongoDB user" "${BACKUP_USER_NAME}"
 # env: MONGODB_PASSWORD: The password of the MongoDB user. Defaults to ${BACKUP_USER_PASSWORD}.
